efd7ffb0cf6dbcf65bde55f100b9e18e66868a14
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-21 21:54:08 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,8 +1,1 @@
-df_derivadas["codmes"] = df_derivadas["FechaAsignacion"].dt.strftime("%Y%m")
-
-C:\Users\T72496\AppData\Local\Temp\ipykernel_6308\3014719524.py:1: SettingWithCopyWarning: 
-A value is trying to be set on a copy of a slice from a DataFrame.
-Try using .loc[row_indexer,col_indexer] = value instead
-
-See the caveats in the documentation: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
-  df_derivadas["CODMES"] = df_derivadas["FechaAsignacion"].dt.strftime("%Y%m")
+df_derivadas.loc[:, "codmes"] = df_derivadas["FechaAsignacion"].dt.strftime("%Y%m")
