bc683bf95c8494f9e8d526f2131f4eb1fda93133
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-21 21:32:21 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,21 +1,6 @@
-import pandas as pd
-import glob
-import os
+valores_incluir = ["Activo", "Pendiente"]
+df_filtrado_incluir = df_unido[df_unido["Estado"].isin(valores_incluir)]
 
-# Ruta de la carpeta
-ruta = "INPUT/POWERAPP"
 
-# Patrón de archivos (ajústalo si cambia el nombre base)
-patron = os.path.join(ruta, "1n_Apps_2025*.csv")
-
-# Lista de archivos que coinciden con el patrón
-archivos = glob.glob(patron)
-
-# Leer y concatenar todos los CSV
-dataframes = [pd.read_csv(archivo) for archivo in archivos]
-df_unido = pd.concat(dataframes, ignore_index=True)
-
-# Verificamos el resultado
-print(f"Se han unido {len(archivos)} archivos.")
-print(df_unido.shape)
-print(df_unido.head())
+valores_excluir = ["Inactivo", "Cancelado"]
+df_filtrado_excluir = df_unido[~df_unido["Estado"].isin(valores_excluir)]
