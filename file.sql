1398851e82da73e2afc08e406c46a7d54fe5af40
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-23 16:43:26 -0500

    Update file.sql

--- file.sql
+++ file.sql
@@ -1,9 +1,7 @@
-La siguiente etapa del pipeline es esta:
-df_diario["FLGPENDIENTE"] = 0
+df_pendientes_tcstock_final = df_pendientes_tcstock_final.rename(columns={
+    "FECINICIOPASO": "FECHAHORA"
+})
 
-df_final_validado = pd.concat(
-    [df_diario, df_pendientes_tcstock_final, df_pendientes_cef_final],
-    ignore_index=True
-)
-
-df_final_validado["FLGPENDIENTE"] = df_final_validado["FLGPENDIENTE"].map({0: "NO", 1: "SI"})
+df_pendientes_cef_final = df_pendientes_cef_final.rename(columns={
+    "FECINICIOEVALUACION": "FECHAHORA"
+})
