1d0d61aa57dc9996b48db8b204e77e6662038ebd
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-23 16:27:55 -0500

    Update file.sql

--- file.sql
+++ file.sql
@@ -1,28 +1,11 @@
-df_diario["ANALISTA"] = df_diario["ANALISTA"].astype(str).str.upper().str.strip()
-
-df_pendientes_tcstock_final["FECHA"] = pd.to_datetime(
-    df_pendientes_tcstock_final["FECHA"], dayfirst=True, errors="coerce"
-).dt.date
-
-df_pendientes_cef_final["FECHA"] = pd.to_datetime(
-    df_pendientes_cef_final["FECHA"], dayfirst=True, errors="coerce"
-).dt.date
-
-df_pendientes_tcstock_sin_validar = df_pendientes_tcstock_final.copy()
-df_pendientes_cef_sin_validar = df_pendientes_cef_final.copy()
-
-trabajo_dias = (
-    df_diario[["ANALISTA", "FECHA"]]
-    .drop_duplicates()
-    .assign(TRABAJO=1)
-)
-
-df_pendientes_tcstock_final = df_pendientes_tcstock_final.merge(trabajo_dias, on=["ANALISTA", "FECHA"], how="left")
-df_pendientes_tcstock_final["TRABAJO"] = df_pendientes_tcstock_final["TRABAJO"].fillna(0)
-df_pendientes_tcstock_final["FLGPENDIENTE"] = np.where(df_pendientes_tcstock_final["TRABAJO"] == 1, 1, 0)
-df_pendientes_tcstock_final.drop(columns=["TRABAJO"], inplace=True)
-
-df_pendientes_cef_final = df_pendientes_cef_final.merge(trabajo_dias, on=["ANALISTA", "FECHA"], how="left")
-df_pendientes_cef_final["TRABAJO"] = df_pendientes_cef_final["TRABAJO"].fillna(0)
-df_pendientes_cef_final["FLGPENDIENTE"] = np.where(df_pendientes_cef_final["TRABAJO"] == 1, 1, 0)
-df_pendientes_cef_final.drop(columns=["TRABAJO"], inplace=True)
+for name in ["df_pendientes_tcstock_final", "df_pendientes_cef_final"]:
+    df = locals()[name]
+    df = (
+        df.merge(trabajo_dias, on=["ANALISTA", "FECHA"], how="left")
+          .assign(
+              TRABAJO=lambda x: x["TRABAJO"].fillna(0),
+              FLGPENDIENTE=lambda x: np.where(x["TRABAJO"] == 1, 1, 0)
+          )
+          .drop(columns=["TRABAJO"])
+    )
+    locals()[name] = df
