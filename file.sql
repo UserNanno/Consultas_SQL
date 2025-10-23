85d666dfad0a4483bb2ccbce288e353d9062f235
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-23 16:28:30 -0500

    Update file.sql

--- file.sql
+++ file.sql
@@ -1,11 +1,20 @@
-for name in ["df_pendientes_tcstock_final", "df_pendientes_cef_final"]:
-    df = locals()[name]
-    df = (
-        df.merge(trabajo_dias, on=["ANALISTA", "FECHA"], how="left")
-          .assign(
-              TRABAJO=lambda x: x["TRABAJO"].fillna(0),
-              FLGPENDIENTE=lambda x: np.where(x["TRABAJO"] == 1, 1, 0)
-          )
-          .drop(columns=["TRABAJO"])
-    )
-    locals()[name] = df
+df_pendientes_tcstock_final = (
+    df_pendientes_tcstock_final
+      .merge(trabajo_dias, on=["ANALISTA", "FECHA"], how="left")
+      .assign(
+          TRABAJO=lambda x: x["TRABAJO"].fillna(0),
+          FLGPENDIENTE=lambda x: np.where(x["TRABAJO"] == 1, 1, 0)
+      )
+      .drop(columns=["TRABAJO"])
+)
+
+
+df_pendientes_cef_final = (
+    df_pendientes_cef_final
+      .merge(trabajo_dias, on=["ANALISTA", "FECHA"], how="left")
+      .assign(
+          TRABAJO=lambda x: x["TRABAJO"].fillna(0),
+          FLGPENDIENTE=lambda x: np.where(x["TRABAJO"] == 1, 1, 0)
+      )
+      .drop(columns=["TRABAJO"])
+)
