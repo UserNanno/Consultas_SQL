c637e6499c9b9d850d9380169062373c1ace8308
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-23 16:39:55 -0500

    Update file.sql

--- file.sql
+++ file.sql
@@ -1,22 +1,9 @@
-TCStock coincidencias: 11 (55.0% del total de pendientes únicos)
-CEF coincidencias: 14 (87.5% del total de pendientes únicos)
+La siguiente etapa del pipeline es esta:
+df_diario["FLGPENDIENTE"] = 0
 
-TCStock: 9 pendientes sin registro en df_diario (9 analistas)
+df_final_validado = pd.concat(
+    [df_diario, df_pendientes_tcstock_final, df_pendientes_cef_final],
+    ignore_index=True
+)
 
-Analistas con pendientes sin registro:
-  - JESSICA FARRO CRUZ: 1 pendiente(s)
-  - FRANKEL MAYERS CHUQUICHAICO RECUAY: 1 pendiente(s)
-  - LOURDES AGUILAR TORRE: 1 pendiente(s)
-  - JACQUELINE IZAGUIRRE PUMACAYO: 1 pendiente(s)
-  - NARDA TRIGUEROS CRUZ: 1 pendiente(s)
-  - MARVIN VIRHUEZ BLANCAS: 1 pendiente(s)
-  - GABRIELA VILCHEZ RAMOS: 1 pendiente(s)
-  - (ANALISTA VACÍO): 1 pendiente(s)
-  - CONNIE YUPANQUI R.: 1 pendiente(s)
-
-
-CEF: 2 pendientes sin registro en df_diario (2 analistas)
-
-Analistas con pendientes sin registro:
-  - JOSE PACHECO V.: 1 pendiente(s)
-  - BARBARA PRADO MAZA: 1 pendiente(s)
+df_final_validado["FLGPENDIENTE"] = df_final_validado["FLGPENDIENTE"].map({0: "NO", 1: "SI"})
