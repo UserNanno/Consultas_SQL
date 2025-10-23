33707c035068472e30cdb7fe4a3f3858226ee8bb
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-23 16:30:25 -0500

    Update file.sql

--- file.sql
+++ file.sql
@@ -1,24 +1,9 @@
-trabajo_dias = (
-    df_diario[["ANALISTA", "FECHA"]]
-    .drop_duplicates()
-    .assign(TRABAJO=1)
-)
-df_pendientes_tcstock_final = (
-    df_pendientes_tcstock_final
-      .merge(trabajo_dias, on=["ANALISTA", "FECHA"], how="left")
-      .assign(
-          TRABAJO=lambda x: x["TRABAJO"].fillna(0),
-          FLGPENDIENTE=lambda x: np.where(x["TRABAJO"] == 1, 1, 0)
-      )
-      .drop(columns=["TRABAJO"])
-)
+print("TCStock coincidencias:", (pd.merge(
+    df_pendientes_tcstock_final[["ANALISTA","FECHA"]].drop_duplicates(),
+    trabajo_dias, on=["ANALISTA","FECHA"], how="inner"
+).shape[0]))
 
-df_pendientes_cef_final = (
-    df_pendientes_cef_final
-      .merge(trabajo_dias, on=["ANALISTA", "FECHA"], how="left")
-      .assign(
-          TRABAJO=lambda x: x["TRABAJO"].fillna(0),
-          FLGPENDIENTE=lambda x: np.where(x["TRABAJO"] == 1, 1, 0)
-      )
-      .drop(columns=["TRABAJO"])
-)
+print("CEF coincidencias:", (pd.merge(
+    df_pendientes_cef_final[["ANALISTA","FECHA"]].drop_duplicates(),
+    trabajo_dias, on=["ANALISTA","FECHA"], how="inner"
+).shape[0]))
