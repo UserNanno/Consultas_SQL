f758443a185b0b119b0822acc817d6165a2c00b1
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-23 19:40:25 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,20 +1,2 @@
-import pandas as pd
-import glob
-import os
-
-# Ruta donde están tus archivos CSV
-ruta = "INPUT/POWERAPP/"
-
-# Buscar todos los archivos CSV que empiecen con '1n_Apps_2025'
-archivos = glob.glob(os.path.join(ruta, "1n_Apps_2025*.csv"))
-
-# Leer y concatenar todos los CSV en un solo DataFrame
-df = pd.concat((pd.read_csv(f) for f in archivos), ignore_index=True)
-
-# Mostrar los nombres de las columnas para ver cuáles quieres conservar
-# Ejemplo: seleccionar solo algunas columnas
-columnas_deseadas = ['Nombre', 'ID', 'Fecha', 'Estado']  # cambia por tus columnas reales
-df_filtrado = df[columnas_deseadas]
-
-# Ver los primeros registros
-df_filtrado.head()
+cols_selected = ['Title', 'FechaAsignacion', 'Tipo de Producto', 'ResultadoAnalista', 'Analista', 'Motivo Resultado Analista', 'AñoMes', 'Created', 'Motivo_MD', 'Submotivo_MD']  # cambia por tus columnas reales
+tp_powerapp = df[cols_selected]
