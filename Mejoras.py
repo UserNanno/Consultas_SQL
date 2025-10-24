f3dec33ab92c2c5789c8e53224a58bb8f212808e
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-23 20:16:32 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,19 +1,2 @@
-import pandas as pd
-import glob
-import os
-
-# Ruta donde están los archivos Excel
-PATH_ORGANICO = "INPUT/ORGANICO/"
-FILES_XLSX = glob.glob(os.path.join(PATH_ORGANICO, "1n_Activos_2025*.xlsx"))
-
-if not FILES_XLSX:
-    raise FileNotFoundError("⚠️ No se encontraron archivos 1n_Activos_2025*.xlsx en INPUT/ORGANICO/")
-
-# Leer y concatenar todos los archivos
-df_organico = pd.concat(
-    (pd.read_excel(f, sheet_name=0) for f in FILES_XLSX),
-    ignore_index=True
-)
-
-# Mostrar las primeras filas para verificar
-df_organico.head()
+cols_selected = ['CODMES', 'Matrícula', 'Código Area/Tribu/COE', 'Area/Tribu/COE', 'Código Servicio/Tribu/COE', 'Servicio/Tribu/COE', 'Código Unidad Organizativa', 'Unidad Organizativa', 'Código Agencia', 'Agencia', 'Código Función', 'Función', 'Matrícula Superior']  # cambia por tus columnas reales
+df_organico = df_organico[cols_selected]
