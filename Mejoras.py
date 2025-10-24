79f539595c85af5808722d6699251199cf6c7db2
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-23 19:55:08 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,19 +1,4 @@
-import unicodedata
-
-# Función para eliminar tildes de texto
-def quitar_tildes(s):
-    if isinstance(s, str):
-        return ''.join(
-            c for c in unicodedata.normalize('NFD', s)
-            if unicodedata.category(c) != 'Mn'
-        )
-    return s
-
-# Aplicar a todo el DataFrame solo en columnas de texto
-for col in tp_powerapp.select_dtypes(include=['object']).columns:
-    tp_powerapp[col] = tp_powerapp[col].apply(quitar_tildes)
-
-
-cols_texto = ['ANALISTA', 'PRODUCTO', 'RESULTADOANALISTA', 'SUBMOTIVOMALADERIVACION']
-for col in cols_texto:
-    tp_powerapp[col] = tp_powerapp[col].apply(quitar_tildes)
+tp_powerapp["RESULTADOANALISTA"].unique()
+array(['DENEGADO POR ANALISTA DE CREDITO',
+       'APROBADO POR ANALISTA DE CREDITO', 'DEVOLVER AL GESTOR', 'FACA'],
+      dtype=object)
