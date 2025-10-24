ec2bac5419d01e3adb85c79c60b26e072ce5f67d
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-10-23 20:17:35 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,2 +1,15 @@
-cols_selected = ['CODMES', 'Matrícula', 'Código Area/Tribu/COE', 'Area/Tribu/COE', 'Código Servicio/Tribu/COE', 'Servicio/Tribu/COE', 'Código Unidad Organizativa', 'Unidad Organizativa', 'Código Agencia', 'Agencia', 'Código Función', 'Función', 'Matrícula Superior']  # cambia por tus columnas reales
-df_organico = df_organico[cols_selected]
+df_organico = df_organico[cols_selected].rename(columns={
+    'CODMES': 'CODMES',
+    'Matrícula': 'MATRICULA',
+    'Código Area/Tribu/COE': 'COD_AREA_TRIBU_COE',
+    'Area/Tribu/COE': 'AREA_TRIBU_COE',
+    'Código Servicio/Tribu/COE': 'COD_SERVICIO_TRIBU_COE',
+    'Servicio/Tribu/COE': 'SERVICIO_TRIBU_COE',
+    'Código Unidad Organizativa': 'COD_UNIDAD_ORG',
+    'Unidad Organizativa': 'UNIDAD_ORG',
+    'Código Agencia': 'COD_AGENCIA',
+    'Agencia': 'AGENCIA',
+    'Código Función': 'COD_FUNCION',
+    'Función': 'FUNCION',
+    'Matrícula Superior': 'MATRICULA_SUPERIOR'
+})
