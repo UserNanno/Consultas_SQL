07db947ecb90865299c5da394f8cc976ef1dfa8d
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-11-18 15:32:04 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,10 +1,9 @@
-CREATE TABLE CATALOG_LHCL_PROD_BCP.BCP_EDV_RBMBDN.T72496_BASEOPORTUNIDADES
-LOCATION 'abfss://bcp-edv-rbmbdn@adlscu1lhclbackp05.dfs.core.windows.net/T72496/TABLAS_DELTA/BASE'
+CREATE TABLE CATALOG_LHCL_PROD_BCP_EXPL.BCP_EDV_SEGPER.T70725_CLIENTES_REDEEMERS
 AS
-SELECT * FROM read_files(
-  'abfss://bcp-edv-rbmbdn@adlscu1lhclbackp05.dfs.core.windows.net/T72496/CARGA/BASE/BaseInicial.csv',
-  format => 'csv',
-  header => true,
-  delimiter => ',',
-  mode => 'FAILFAST'
+SELECT *
+FROM read_files(
+ 'abfss://bcp-edv-segper@adlscu1lhclbackp05.dfs.core.windows.net/T70725/EXPORTS/ClientesRedeemers/',
+ format => 'parquet'
 );
+
+[CF_USE_DELTA_FORMAT] Reading from a Delta table is not supported with this syntax. If you would like to consume data from Delta, please refer to the docs: read a Delta table (https://docs.microsoft.com/azure/databricks/delta/tutorial#read), or read a Delta table as a stream source (https://docs.microsoft.com/azure/databricks/structured-streaming/delta-lake#table-streaming-reads-and-writes). The streaming source from Delta is already optimized for incremental consumption of data. SQLSTATE: 42000
