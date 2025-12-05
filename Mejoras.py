0db07d68b0883282829790de7bae5c42ee99d918
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-05 15:43:54 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,49 +1,16 @@
 from pyspark.sql import functions as F
 
-def parse_fecha_hora_esp_col(col):
-    # Aseguramos string
-    s = F.col(col).cast("string")
-    
-    # 1) Normalizar espacios (incluyendo espacios no separables de Salesforce)
-    # Reemplazamos NBSP y NARROW NBSP por espacio normal
-    s = F.regexp_replace(s, u'\u00A0', ' ')
-    s = F.regexp_replace(s, u'\u202F', ' ')
-    
-    # Pasamos a minúsculas y recortamos
-    s = F.lower(F.trim(s))
-    
-    # Colapsar espacios múltiples en uno
-    s = F.regexp_replace(s, r'\s+', ' ')
-    
-    # 2) Normalizar "a. m." / "p. m." -> AM / PM
-    # \W = "no palabra": puntos, espacios, NBSP, etc.
-    s = F.regexp_replace(s, r'(?i)a\W*m\W*', 'AM')
-    s = F.regexp_replace(s, r'(?i)p\W*m\W*', 'PM')
-
-    # 3) Parsear al timestamp con formato día/mes/año + hora 12h
-    # dd/MM/yyyy -> día/mes/año
-    # hh:mm      -> hora 12h
-    # a          -> AM/PM
-    return F.to_timestamp(s, 'dd/MM/yyyy hh:mm a')
-
-
-
 df_salesforce_estados = (
     df_salesforce_estados
-        .withColumn(
-            "FECHORINICIOEVALUACION",
-            parse_fecha_hora_esp_col("FECINICIOEVALUACION")
-        )
-        .withColumn(
-            "FECHORFINEVALUACION",
-            parse_fecha_hora_esp_col("FECFINEVALUACION")
-        )
+        # EXTRAER FECHA
+        .withColumn("FECINICIOEVALUACION", F.to_date("FECHORINICIOEVALUACION"))
+        .withColumn("FECFINEVALUACION", F.to_date("FECHORFINEVALUACION"))
+
+        # EXTRAER HORA (en string HH:mm:ss)
+        .withColumn("HORINICIOEVALUACION", F.date_format("FECHORINICIOEVALUACION", "HH:mm:ss"))
+        .withColumn("HORFINEVALUACION", F.date_format("FECHORFINEVALUACION", "HH:mm:ss"))
+
+        # CODMESEVALUACION = YYYYMM
+        .withColumn("CODMESEVALUACION",
+                    F.date_format("FECINICIOEVALUACION", "yyyyMM"))
 )
-
-
-
-
-df_salesforce_estados.select(
-    "FECINICIOEVALUACION",
-    parse_fecha_hora_esp_col("FECINICIOEVALUACION").alias("PARS_FECHORINICIO")
-).show(truncate=False)
