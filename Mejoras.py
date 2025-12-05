1f27b321bf085e8aa5ff247ee1f124313c3a788d
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-05 14:43:02 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,27 +1,9 @@
 from pyspark.sql import functions as F
 
-# 1. Ruta de los archivos CSV (ajusta según tu entorno)
-salesforce_path = "dbfs:/INPUT/SALESFORCE/INFORME_ESTADO_*.csv"
-
-# 2. Lectura de los CSV de Salesforce
-df_salesforce_raw = (
-    spark.read
-        .format("csv")
-        .option("header", "true")
-        .option("encoding", "ISO-8859-1")  # equivalente a latin1
-        .load(salesforce_path)
-)
-
-# 3. Selección y renombrado de columnas
 df_salesforce = (
-    df_salesforce_raw
-        .select(
-            F.col("Nombre del registro").alias("CODSOLICITUD"),
-            F.col("Estado").alias("ESTADOSOLICITUD"),
-            F.col("Fecha de inicio del paso").alias("FECINICIOEVALUACION"),
-            F.col("Fecha de finalización del paso").alias("FECFINEVALUACION"),
-            F.col("Paso: Nombre").alias("NBRPASO"),
-            F.col("Último actor: Nombre completo").alias("NBRANALISTA"),
-            F.col("Proceso de aprobación: Nombre").alias("PROCESO"),
-        )
+    df_salesforce
+        .withColumn("ESTADOSOLICITUD", F.upper(F.col("ESTADOSOLICITUD")))
+        .withColumn("NBRPASO", F.upper(F.col("NBRPASO")))
+        .withColumn("NBRANALISTA", F.upper(F.col("NBRANALISTA")))
+        .withColumn("PROCESO", F.upper(F.col("PROCESO")))
 )
