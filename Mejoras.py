230379013636acb0da452f8a46b340f07548828b
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-05 15:46:15 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,16 +1,6 @@
-from pyspark.sql import functions as F
-
-df_salesforce_estados = (
+df_repetidas = (
     df_salesforce_estados
-        # EXTRAER FECHA
-        .withColumn("FECINICIOEVALUACION", F.to_date("FECHORINICIOEVALUACION"))
-        .withColumn("FECFINEVALUACION", F.to_date("FECHORFINEVALUACION"))
-
-        # EXTRAER HORA (en string HH:mm:ss)
-        .withColumn("HORINICIOEVALUACION", F.date_format("FECHORINICIOEVALUACION", "HH:mm:ss"))
-        .withColumn("HORFINEVALUACION", F.date_format("FECHORFINEVALUACION", "HH:mm:ss"))
-
-        # CODMESEVALUACION = YYYYMM
-        .withColumn("CODMESEVALUACION",
-                    F.date_format("FECINICIOEVALUACION", "yyyyMM"))
+        .groupBy("CODSOLICITUD")
+        .count()
+        .withColumnRenamed("count", "REPETICIONES")
 )
