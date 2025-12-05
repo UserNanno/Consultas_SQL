a2070d45efee7ea66462af5f305b581288427c22
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-05 11:35:43 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,8 +1,27 @@
 from pyspark.sql import functions as F
 
-def parse_fecha_hora_esp_col(col):
-    s = F.lower(F.trim(col))
-    s = F.regexp_replace(s, r'\s+', ' ')
-    s = F.regexp_replace(s, r'(?i)a\.?\s*m\.?', 'AM')
-    s = F.regexp_replace(s, r'(?i)p\.?\s*m\.?', 'PM')
-    return F.to_timestamp(s, 'dd/MM/yyyy hh:mm a')
+# 1. Ruta de los archivos CSV (ajusta según tu entorno)
+salesforce_path = "dbfs:/INPUT/SALESFORCE/INFORME_ESTADO_*.csv"
+
+# 2. Lectura de los CSV de Salesforce
+df_salesforce_raw = (
+    spark.read
+        .format("csv")
+        .option("header", "true")
+        .option("encoding", "ISO-8859-1")  # equivalente a latin1
+        .load(salesforce_path)
+)
+
+# 3. Selección y renombrado de columnas
+df_salesforce = (
+    df_salesforce_raw
+        .select(
+            F.col("Nombre del registro").alias("CODSOLICITUD"),
+            F.col("Estado").alias("ESTADOSOLICITUD"),
+            F.col("Fecha de inicio del paso").alias("FECINICIOEVALUACION"),
+            F.col("Fecha de finalización del paso").alias("FECFINEVALUACION"),
+            F.col("Paso: Nombre").alias("NBRPASO"),
+            F.col("Último actor: Nombre completo").alias("NBRANALISTA"),
+            F.col("Proceso de aprobación: Nombre").alias("PROCESO"),
+        )
+)
