95843eff3e4e370bfbe8b3500e0b588d16b85635
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-05 14:55:49 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,13 +1,21 @@
-def quitar_tildes(col):
-    return (F.regexp_replace(F.regexp_replace(F.regexp_replace(F.regexp_replace(F.regexp_replace(col,
-        "[ÁÀÂÄáàâä]", "A"),"[ÉÈÊËéèêë]", "E"),"[ÍÌÎÏíìîï]", "I"),"[ÓÒÔÖóòôö]", "O"),"[ÚÙÛÜúùûü]", "U"))
+from pyspark.sql import functions as F
 
+def parse_fecha_hora_esp_col(col):
+    s = F.lower(F.trim(col))
+    s = F.regexp_replace(s, r'\s+', ' ')
+    s = F.regexp_replace(s, r'(?i)a\.?\s*m\.?', 'AM')
+    s = F.regexp_replace(s, r'(?i)p\.?\s*m\.?', 'PM')
+    return F.to_timestamp(s, 'dd/MM/yyyy hh:mm a')
 
 
 
 df_salesforce = (
     df_salesforce
-        .withColumn("NBRPASO", quitar_tildes("NBRPASO"))
-        .withColumn("NBRANALISTA", quitar_tildes("NBRANALISTA"))
-        .withColumn("PROCESO", quitar_tildes("PROCESO"))
+        .withColumn("FECHORINICIOEVALUACION", parse_fecha_hora_esp_col(F.col("FECINICIOEVALUACION")))
+        .withColumn("FECHORFINEVALUACION",    parse_fecha_hora_esp_col(F.col("FECFINEVALUACION")))
+        .withColumn("FECINICIOEVALUACION", F.to_date("FECHORINICIOEVALUACION"))
+        .withColumn("FECFINEVALUACION",   F.to_date("FECHORFINEVALUACION"))
+        .withColumn("HORINICIOEVALUACION", F.date_format("FECHORINICIOEVALUACION", "HH:mm:ss"))
+        .withColumn("HORFINEVALUACION",   F.date_format("FECHORFINEVALUACION", "HH:mm:ss"))
+        .withColumn("CODMESEVALUACION",   F.date_format("FECINICIOEVALUACION", "yyyyMM"))
 )
