688bc4b85c2a7955f1df89332a648bcdf34e319b
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-05 14:59:12 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,21 +1,5 @@
-from pyspark.sql import functions as F
+Asi estan mira
 
-def parse_fecha_hora_esp_col(col):
-    s = F.lower(F.trim(col))
-    s = F.regexp_replace(s, r'\s+', ' ')
-    s = F.regexp_replace(s, r'(?i)a\.?\s*m\.?', 'AM')
-    s = F.regexp_replace(s, r'(?i)p\.?\s*m\.?', 'PM')
-    return F.to_timestamp(s, 'dd/MM/yyyy hh:mm a')
+01/04/2025 10:31 a. m.
 
-
-
-df_salesforce = (
-    df_salesforce
-        .withColumn("FECHORINICIOEVALUACION", parse_fecha_hora_esp_col(F.col("FECINICIOEVALUACION")))
-        .withColumn("FECHORFINEVALUACION",    parse_fecha_hora_esp_col(F.col("FECFINEVALUACION")))
-        .withColumn("FECINICIOEVALUACION", F.to_date("FECHORINICIOEVALUACION"))
-        .withColumn("FECFINEVALUACION",   F.to_date("FECHORFINEVALUACION"))
-        .withColumn("HORINICIOEVALUACION", F.date_format("FECHORINICIOEVALUACION", "HH:mm:ss"))
-        .withColumn("HORFINEVALUACION",   F.date_format("FECHORFINEVALUACION", "HH:mm:ss"))
-        .withColumn("CODMESEVALUACION",   F.date_format("FECINICIOEVALUACION", "yyyyMM"))
-)
+12/05/2025 04:59 p. m.
