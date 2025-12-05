b1aa8efd833d1728e14e632c11d8caf84c88b37f
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-05 15:30:35 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,1 +1,15 @@
- [UNRESOLVED_COLUMN.WITH_SUGGESTION] A column or function parameter with name `FECINICIOEVALUACION_TXT` cannot be resolved. Did you mean one of the following? [`FECINICIOEVALUACION`, `FECHORINICIOEVALUACION`, `HORINICIOEVALUACION`, `FECFINEVALUACION`, `FECHORFINEVALUACION`].;
+from pyspark.sql import functions as F
+
+def parse_fecha_hora_esp_col(col):
+    s = F.lower(F.trim(col))
+    s = F.regexp_replace(s, r'\s+', ' ')
+    s = F.regexp_replace(s, r'(?i)a\.?\s*m\.?', 'AM')
+    s = F.regexp_replace(s, r'(?i)p\.?\s*m\.?', 'PM')
+    return F.to_timestamp(s, 'dd/MM/yyyy hh:mm a')
+
+
+df_salesforce = (
+    df_salesforce
+        .withColumn("FECHORINICIOEVALUACION", parse_fecha_hora_esp_col(F.col("FECINICIOEVALUACION")))
+        .withColumn("FECHORFINEVALUACION", parse_fecha_hora_esp_col(F.col("FECFINEVALUACION")))
+)
