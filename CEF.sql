9468b9ccbd87245adcb8e75313f4f53ce8f34f5c
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-09 17:08:05 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,26 +1,6 @@
-# 1️⃣ Cantidad de registros en el DF original
-count_original = df_salesforce_estados.count()
-print("📌 Registros en df_salesforce_estados (original):", count_original)
-
-# 2️⃣ Cantidad de registros únicos (solo el último por solicitud)
-count_unicos = df_salesforce_estados_unicos.count()
-print("📌 Registros en df_salesforce_estados_unicos:", count_unicos)
-
-# 3️⃣ Cantidad de registros descartados (los repetidos)
-count_descartados = df_salesforce_estados_descartados.count()
-print("📌 Registros en df_salesforce_estados_descartados:", count_descartados)
-
-
-# 4️⃣ Cantidad de repetidos por CODSOLICITUD
-df_repetidos_count = (
+df_repeticiones = (
     df_salesforce_estados
         .groupBy("CODSOLICITUD")
         .count()
-        .filter(F.col("count") > 1)
+        .withColumnRenamed("count", "CANTIDAD_REPETICIONES")
 )
-
-count_codsolicitud_repetidos = df_repetidos_count.count()
-
-print("📌 Cantidad de CODSOLICITUD con repeticiones:", count_codsolicitud_repetidos)
-print("📌 Ejemplo de valores repetidos:")
-df_repetidos_count.show(10, truncate=False)
