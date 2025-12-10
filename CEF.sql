f9086a93a0f86589a11063ae5fad5125e30d3e7c
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-09 23:53:47 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,12 +1,5 @@
-# === 4. CREATE TABLA DELTA ===
-(df.write.format("delta")
-   .partitionBy("CODLOTEOFERTA")
-   .mode("overwrite")
-   .save(tabla_delta_path))
-
-
-spark.sql(f"""
-    CREATE TABLE IF NOT EXISTS CATALOG_LHCL_PROD_BCP.BCP_EDV_RBMBDN.HM_LEADS_BDI
-    USING DELTA
-    LOCATION '{tabla_delta_path}'
-""")
+df.write \
+  .format("delta") \
+  .mode("overwrite") \
+  .option("overwriteSchema", "true") \
+  .saveAsTable("CATALOG_LHCL_PROD_BCP.BCP_EDV_RBMBDN.HM_LEADS_BDI")
