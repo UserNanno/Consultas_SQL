f21c0cc34813b16416c06e9296433cc29084300a
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-10 12:08:10 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,23 +1,13 @@
-df_salesforce_enriq.write \
-    .format("delta") \
-    .mode("overwrite") \
-    .option("overwriteSchema", "true") \
-    .partitionBy("CODMESEVALUACION") \
-    .saveAsTable("CATALOG_LHCL_PROD_BCP.BCP_EDV_RBMBDN.HM_LEADS_BDI")
+df_salesforce_productos = (
+    df_salesforce_raw
+        .select(
+            F.col("Nombre de la oportunidad").alias("CODOPORTUNIDAD"),
+            F.col("Nombre del Producto").alias("NBRPRODUCTO"),
+            F.col("Etapa").alias("ETAPA"),
+            F.col("Analista de crédito").alias("NBRANALISTA"),
+            F.col("Tipo de Acción").alias("TIPACCION")
+        )
+)
 
 
-
-
-df_salesforce_enriq.filter(F.col("MATORGANICO").isNull()).count()
-
-
-
-df_salesforce_enriq \
-    .filter(F.col("MATORGANICO").isNull()) \
-    .select(
-        "CODMESEVALUACION",
-        "CODSOLICITUD",
-        "NBRULTACTOR",
-        "NBRULTACTORPASO"
-    ) \
-    .show(50, truncate=False)
+Tengo este otro df donde quiero hacer lo mismo con NBRANALISTA que haciamos en df_salesforce_estados para buscar su MATORGANICO y colocarlo acá. No habría problema que haya por ejemplo haya 3 nombres en algunos registros donde se repite el segundo nombre con nuestra forma de identificar con la tolerancia, no habria problemas no para aplicar lo que ya tenemos?
