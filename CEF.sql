65840ae76f5f694fde82296d8b9cfe998378ff6f
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-10 00:07:01 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,12 +1,23 @@
-df_final = df_salesforce_enriq.select(
-    "CODMESEVALUACION",
-    "CODSOLICITUD",
-    "NBRULTACTOR",
-    "PROCESO",
-    "NBRPASO",
-    "ESTADOSOLICITUD",
-    "FECHORINICIOEVALUACION",
-    "FECHORFINEVALUACION",
-    "MATORGANICO",
-    "MATSUPERIOR"
-)
+df_salesforce_enriq.write \
+    .format("delta") \
+    .mode("overwrite") \
+    .option("overwriteSchema", "true") \
+    .partitionBy("CODMESEVALUACION") \
+    .saveAsTable("CATALOG_LHCL_PROD_BCP.BCP_EDV_RBMBDN.HM_LEADS_BDI")
+
+
+
+
+df_salesforce_enriq.filter(F.col("MATORGANICO").isNull()).count()
+
+
+
+df_salesforce_enriq \
+    .filter(F.col("MATORGANICO").isNull()) \
+    .select(
+        "CODMESEVALUACION",
+        "CODSOLICITUD",
+        "NBRULTACTOR",
+        "NBRULTACTORPASO"
+    ) \
+    .show(50, truncate=False)
