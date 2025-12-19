6a10f6630b4c360c2de1197b38d2a2012c694885
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-19 06:28:35 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,15 +1,3 @@
-from pyspark.sql import functions as F
-
-df_dbg = df_sf_estados_raw.withColumn("FILE", F.input_file_name())
-
-df_dbg.filter(F.col("Nombre del registro") == "O0018721853") \
-  .select(
-      "FILE",
-      "Nombre del registro",
-      "Paso: Nombre",
-      "Estado",
-      "Estado del paso",
-      "Fecha de inicio del paso",
-      "Fecha de finalización del paso"
-  ) \
-  .show(truncate=False)
+display(df_sf_estados_raw \
+    .filter(F.col("Nombre del registro") == "O0018721853") \
+    .show(truncate=False))
