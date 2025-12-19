043a6c224e7bc25165c6444e62dbe316e9f7ed9d
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-19 06:24:43 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,3 +1,8 @@
-for c in df_sf_estados_raw.columns:
-    if "estado" in c.lower() and "paso" in c.lower():
-        print(repr(c))
+from pyspark.sql import functions as F
+
+df_dbg = df_sf_estados_raw.withColumn("FILE", F.input_file_name())
+
+df_dbg.filter(F.col("FILE").like("%INFORME_ESTADO_4%")) \
+  .select("FILE", "Nombre del registro", "Paso: Nombre", "Estado del paso") \
+  .filter(F.col("Nombre del registro")=="O0018721853") \
+  .show(truncate=False)
