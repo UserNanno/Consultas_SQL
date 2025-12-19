1c844ca6da794be36c3c0327cb7de884a0e84aaa
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-19 18:38:11 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,2 +1,36 @@
-opts.add_argument("--window-size=1200,800")
-opts.add_argument("--window-position=-32000,-32000")  # 👈 OCULTAR
+(venv) D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\WebAutomatic>py main.py
+----
+ruc_consultado: 10788016005
+error: Message: stale element reference: stale element not found
+  (Session info: chrome=143.0.7499.170); For documentation on this error, please visit: https://www.selenium.dev/documentation/webdriver/troubleshooting/errors#staleelementreferenceexception
+Stacktrace:
+Symbols not available. Dumping unresolved backtrace:
+        0x7ff679d488e5
+        0x7ff679d48940
+        0x7ff679b2165d
+        0x7ff679b37ed3
+        0x7ff679b369d3
+        0x7ff679b2a006
+        0x7ff679b27f4c
+        0x7ff679b2c3b4
+        0x7ff679b2c47f
+        0x7ff679b792e5
+        0x7ff679b79d3c
+        0x7ff679b6cd6c
+        0x7ff679b6c746
+        0x7ff679bcac97
+        0x7ff679b6ac29
+        0x7ff679b6ba93
+        0x7ff67a060640
+        0x7ff67a05af80
+        0x7ff67a0796e6
+        0x7ff679d65de4
+        0x7ff679d6ed8c
+        0x7ff679d52004
+        0x7ff679d521b5
+        0x7ff679d37ee2
+        0x7ffeea26259d
+        0x7ffeeae2af78
+
+
+(venv) D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\WebAutomatic>py main.py
