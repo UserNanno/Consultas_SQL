2f3ac0431a6853502ccb66a2121095962646c88f
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-19 06:23:37 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,3 +1,3 @@
-df_salesforce_estados \
-    .filter(F.col("CODSOLICITUD") == "O0018721853") \
-    .show(truncate=False)
+for c in df_sf_estados_raw.columns:
+    if "estado" in c.lower() and "paso" in c.lower():
+        print(repr(c))
