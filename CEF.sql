d1fba4df357bb235ba6cefcac5f7b07156930946
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-23 18:14:30 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,17 +1,23 @@
-pip install pyautogui pillow
-
-
-import pyautogui
-
-# Coordenadas (x, y, ancho, alto)
-# OJO: son píxeles de pantalla
-region = (500, 300, 200, 80)
-
-img = pyautogui.screenshot(region=region)
-
-img.save("captura.png")
+pip install playwright
+playwright install
 
 
 
-import pyautogui
-print(pyautogui.position())
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)  # True si no quieres ver el navegador
+    page = browser.new_page()
+
+    page.goto("https://tu-pagina.com")
+
+    # Espera a que el elemento exista
+    page.wait_for_timeout(5000)  # 2 segundos
+    page.wait_for_selector("#test")
+
+    # Captura SOLO ese elemento
+    page.locator("#test").screenshot(path="captura.png")
+
+    browser.close()
+
+print("Captura guardada como captura.png")
