1d610950d5fa017cb3e73080abc0bea9df7a18a8
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-23 17:43:49 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,3 +1,9 @@
+La pagina es esta
+
+  https://extranet.sbs.gob.pe/app/login.jsp
+
+  el usuario va aca
+
 <input id="c_c_usuario" type="text" name="c_c_usuario" value="" class="WEB_zonaIngresoInput watermark" maxlength="16" title="Ingrese su usuario">
 
 para la contraseña se ingresa aca
@@ -5,3 +11,7 @@ para la contraseña se ingresa aca
 pero se utilizan estos botones
 
 <ul class="WEB_zonaIngresoBotonera" id="ulKeypad"><ul><li onclick="liClick(4)">4</li><li onclick="liClick(7)">7</li><li onclick="liClick(8)">8</li><li onclick="liClick(0)">0</li><li onclick="liClick(9)">9</li><li onclick="liClick(5)">5</li><li onclick="liClick(3)">3</li><li onclick="liClick(2)">2</li><li onclick="liClick(1)">1</li><li onclick="liClick(6)">6</li></ul><li onclick="limpiarCodigo()" class="WEB_zonaIngresobtnBorrar">limpiar</li></ul>
+
+El boton es este
+
+<input type="button" value="ACEPTAR" class="WEB_zonaIngresoBoton" id="btnIngresar" name="btnIngresar" onclick="ingresar()">
