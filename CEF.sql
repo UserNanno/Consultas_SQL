c5e9d3ce6fe87e2f54a42172c45587b59eaaf42d
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-23 18:23:07 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,23 +1,50 @@
-https://github.com/UB-Mannheim/tesseract/wiki
+(venv) D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs>pip install pytesseract opencv-python pillow
+Requirement already satisfied: pytesseract in d:\datos de usuarios\t72496\desktop\modelos_rpts\venv\lib\site-packages (0.3.13)
+Collecting opencv-python
+  Using cached opencv_python-4.12.0.88-cp37-abi3-win_amd64.whl.metadata (19 kB)
+Requirement already satisfied: pillow in d:\datos de usuarios\t72496\desktop\modelos_rpts\venv\lib\site-packages (12.0.0)
+Requirement already satisfied: packaging>=21.3 in d:\datos de usuarios\t72496\desktop\modelos_rpts\venv\lib\site-packages (from pytesseract) (25.0)
+Collecting numpy<2.3.0,>=2 (from opencv-python)
+  Using cached numpy-2.2.6.tar.gz (20.3 MB)
+  Installing build dependencies ... done
+  Getting requirements to build wheel ... done
+  Installing backend dependencies ... done
+  Preparing metadata (pyproject.toml) ... error
+  error: subprocess-exited-with-error
 
-pip install pytesseract opencv-python pillow
+  × Preparing metadata (pyproject.toml) did not run successfully.
+  │ exit code: 1
+  ╰─> [21 lines of output]
+      + D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\venv\Scripts\python.exe C:\Users\T72496\AppData\Local\Temp\pip-install-bj8ahike\numpy_c29ea2ee5ac448c6ac72e9646382bf69\vendored-meson\meson\meson.py setup C:\Users\T72496\AppData\Local\Temp\pip-install-bj8ahike\numpy_c29ea2ee5ac448c6ac72e9646382bf69 C:\Users\T72496\AppData\Local\Temp\pip-install-bj8ahike\numpy_c29ea2ee5ac448c6ac72e9646382bf69\.mesonpy-ekf_7_he -Dbuildtype=release -Db_ndebug=if-release -Db_vscrt=md --native-file=C:\Users\T72496\AppData\Local\Temp\pip-install-bj8ahike\numpy_c29ea2ee5ac448c6ac72e9646382bf69\.mesonpy-ekf_7_he\meson-python-native-file.ini
+      The Meson build system
+      Version: 1.5.2
+      Source dir: C:\Users\T72496\AppData\Local\Temp\pip-install-bj8ahike\numpy_c29ea2ee5ac448c6ac72e9646382bf69
+      Build dir: C:\Users\T72496\AppData\Local\Temp\pip-install-bj8ahike\numpy_c29ea2ee5ac448c6ac72e9646382bf69\.mesonpy-ekf_7_he
+      Build type: native build
+      Project name: NumPy
+      Project version: 2.2.6
+      WARNING: Failed to activate VS environment: Could not find C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe
 
+      ..\meson.build:1:0: ERROR: Unknown compiler(s): [['icl'], ['cl'], ['cc'], ['gcc'], ['clang'], ['clang-cl'], ['pgcc']]
+      The following exception(s) were encountered:
+      Running `icl ""` gave "[WinError 2] El sistema no puede encontrar el archivo especificado"
+      Running `cl /?` gave "[WinError 2] El sistema no puede encontrar el archivo especificado"
+      Running `cc --version` gave "[WinError 2] El sistema no puede encontrar el archivo especificado"
+      Running `gcc --version` gave "[WinError 2] El sistema no puede encontrar el archivo especificado"
+      Running `clang --version` gave "[WinError 2] El sistema no puede encontrar el archivo especificado"
+      Running `clang-cl /?` gave "[WinError 2] El sistema no puede encontrar el archivo especificado"
+      Running `pgcc --version` gave "[WinError 2] El sistema no puede encontrar el archivo especificado"
 
-import pytesseract
-import cv2
+      A full log can be found at C:\Users\T72496\AppData\Local\Temp\pip-install-bj8ahike\numpy_c29ea2ee5ac448c6ac72e9646382bf69\.mesonpy-ekf_7_he\meson-logs\meson-log.txt
+      [end of output]
 
-# ⚠️ Solo si NO lo agregaste al PATH
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+  note: This error originates from a subprocess, and is likely not a problem with pip.
+error: metadata-generation-failed
 
-# Leer imagen en blanco y negro
-img = cv2.imread("captura.png", cv2.IMREAD_GRAYSCALE)
+× Encountered error while generating package metadata.
+╰─> numpy
 
-# Mejorar contraste
-_, img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+note: This is an issue with the package mentioned above, not pip.
+hint: See above for details.
 
-# Configuración OCR (una sola línea, pocos caracteres)
-config = "--psm 7 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-texto = pytesseract.image_to_string(img, config=config)
-
-print(texto.strip())
+(venv) D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs>
