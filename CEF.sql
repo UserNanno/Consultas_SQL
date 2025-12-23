9730690f9d939180023aab9482ac5fefd15f87c5
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-23 18:16:57 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,10 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
 
-driver = webdriver.Edge()
+CHROMEDRIVER_PATH = r"D:\Datos de Usuarios\T72496\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+
+options = Options()
+options.add_argument("--start-maximized")
+
+service = Service(CHROMEDRIVER_PATH)
+driver = webdriver.Chrome(service=service, options=options)
+
 driver.get("https://tu-pagina.com")
 
+# Espera a que cargue el elemento
+time.sleep(2)  # simple y efectivo
 element = driver.find_element(By.ID, "test")
+
+# Captura SOLO el elemento
 element.screenshot("captura.png")
+
+print("Captura guardada como captura.png")
 
 driver.quit()
