309e25a5f430fc35aa2fb34985f5e94871fb468c
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-23 09:17:25 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,4 +1,35 @@
-Se esta aplicando bien lo que te dije?
-Lo que pasa que hay una solicitud donde tiene nombre de paso EVALUACION DE SOLICITUD, Proceso CO SOLICITUD APROBACIONES pero que se repite 2 veces. En la primera vez que aparece es en con la hora mas antigua donde sale NBRULTACTOR la matrícula de Lesly y en NBRULTIMOACTORPASO la matrícula de Lesly nuevamente pero acá lo rechaza. Pasado dos minutos aparece otro registro con la misma solicitud donde en NBRULTACTOR aparece la matrícula de su supervisora pero en NBRULTIACTORPASO aparece la matrícula de Lesly. Y luego ya aparece otra vez la solicitud pero con NBRPASO "EVALUACION DE SOLICITUD APROBADOR".
-En el resultado final me indica que la matrícula fue sacada de PRODUCTOS_NBRANALISTA pero había todo lo necesario para sacarlo desde SF_ESTADOS.
-En este caso debió tomar el registro mas reciente de la solicitud con EVALUACION DE SOLICITUD y CO SOLICITUD APROBACIONES y recién de ahí buscar la matrícula del analista
+Cuando se ejecutó esto
+# 1) STAGING
+df_org = load_organico(spark, BASE_DIR_ORGANICO)
+df_org_tokens = build_org_tokens(df_org)
+df_estados = load_sf_estados(spark, PATH_SF_ESTADOS)
+df_productos = load_sf_productos_validos(spark, PATH_SF_PRODUCTOS)
+df_apps = load_powerapps(spark, PATH_PA_SOLICITUDES)
+# 2) ENRIQUECIMIENTO CON ORGANICO
+df_estados_enriq = enrich_estados_con_organico(df_estados, df_org_tokens)
+df_productos_enriq = enrich_productos_con_organico(df_productos, df_org_tokens)
+
+Hice lo siguiente
+
+display(df_estados_enriq[df_estados_enriq["CODSOLICITUD"]== "O0018169329"])
+
+aca me sale en el primer registro:
+
+NBRULTACTOR -> LESLY
+NBRULTACTORPASO -> LESLY
+MATORGANICO -> S18795 (MATRICULA DE SUPERVISORA DE LESLY)
+MATSUPERIOR -> U17293 (MATRICULA DEL GERENTE)
+MATORGANICOPASO -> S18795
+MATSUPERIORPASO -> U17293
+
+en el segundo registro me sale
+
+NBRULTACTOR -> EVELYN (sUPERVISOR)
+NBRULTACTORPASO -> LESLY
+MATORGANICO -> S18795 (MATRICULA DE SUPERVISORA DE LESLY)
+MATSUPERIOR -> U17293 (MATRICULA DEL GERENTE)
+MATORGANICOPASO -> S18795
+MATSUPERIORPASO -> U17293
+
+
+EL PROBLEMA ES ACÁ QUE NO PEGA BIEN SU MATRÍCULA DEL ANALISTA. POR QUÉ PASA ESO? 
