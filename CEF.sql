6c27369ddafb2bf36fdaf4768b295493a562ace2
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-23 18:14:45 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,23 +1,10 @@
-pip install playwright
-playwright install
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+driver = webdriver.Edge()
+driver.get("https://tu-pagina.com")
 
+element = driver.find_element(By.ID, "test")
+element.screenshot("captura.png")
 
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)  # True si no quieres ver el navegador
-    page = browser.new_page()
-
-    page.goto("https://tu-pagina.com")
-
-    # Espera a que el elemento exista
-    page.wait_for_timeout(5000)  # 2 segundos
-    page.wait_for_selector("#test")
-
-    # Captura SOLO ese elemento
-    page.locator("#test").screenshot(path="captura.png")
-
-    browser.close()
-
-print("Captura guardada como captura.png")
+driver.quit()
