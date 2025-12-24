2453acdf0c6927f072494d7ef2e349d994f010e8
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-23 19:30:27 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,1 +1,22 @@
-https://tesseract-ocr.github.io/tessdoc/Compiling.html#macos
+import pytesseract
+from PIL import Image
+import os
+import re
+
+BASE = r"C:\Users\usern\Downloads\Data\Ncesario"
+
+pytesseract.pytesseract.tesseract_cmd = os.path.join(BASE, "tesseract.exe")
+os.environ["TESSDATA_PREFIX"] = BASE
+
+img = Image.open("captura.png")
+
+config = (
+    r'--oem 3 --psm 8 '
+    r'-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+)
+
+text = pytesseract.image_to_string(img, config=config)
+text = re.sub(r"\s+", "", text)
+text = text[:4]
+
+print("OCR:", text)
