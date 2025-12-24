3ac27c2c02f8196727800b23c4cc09accc1e4227
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-23 20:45:11 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,22 +1,23 @@
-import pytesseract
-from PIL import Image
-import os
-import re
+(venv) D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\WebAutomatic>py reconocimiento.py
+Traceback (most recent call last):
+  File "D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\WebAutomatic\reconocimiento.py", line 18, in <module>
+    text = pytesseract.image_to_string(img, config=config)
+  File "D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\venv\Lib\site-packages\pytesseract\pytesseract.py", line 486, in image_to_string
+    return {
+           ~
+    ...<2 lines>...
+        Output.STRING: lambda: run_and_get_output(*args),
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    }[output_type]()
+    ~~~~~~~~~~~~~~^^
+  File "D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\venv\Lib\site-packages\pytesseract\pytesseract.py", line 489, in <lambda>
+    Output.STRING: lambda: run_and_get_output(*args),
+                           ~~~~~~~~~~~~~~~~~~^^^^^^^
+  File "D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\venv\Lib\site-packages\pytesseract\pytesseract.py", line 352, in run_and_get_output
+    run_tesseract(**kwargs)
+    ~~~~~~~~~~~~~^^^^^^^^^^
+  File "D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\venv\Lib\site-packages\pytesseract\pytesseract.py", line 284, in run_tesseract
+    raise TesseractError(proc.returncode, get_errors(error_string))
+pytesseract.pytesseract.TesseractError: (1, 'Error opening data file D:\\Datos de Usuarios\\T72496\\Desktop\\MODELOS_RPTs\\WebAutomatic\\Helpers\\tesseract/eng.traineddata Please make sure the TESSDATA_PREFIX environment variable is set to your "tessdata" directory. Failed loading language \'eng\' Tesseract couldn\'t load any languages! Could not initialize tesseract.')
 
-BASE = r"C:\Users\usern\Downloads\Data\Ncesario"
-
-pytesseract.pytesseract.tesseract_cmd = os.path.join(BASE, "tesseract.exe")
-os.environ["TESSDATA_PREFIX"] = BASE
-
-img = Image.open("captura.png")
-
-config = (
-    r'--oem 3 --psm 8 '
-    r'-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-)
-
-text = pytesseract.image_to_string(img, config=config)
-text = re.sub(r"\s+", "", text)
-text = text[:4]
-
-print("OCR:", text)
+(venv) D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs\WebAutomatic>
