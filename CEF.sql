f27ec7743580c32a851f6b1f54aa5f41cfec16ea
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-26 11:27:54 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,6 +1,1 @@
-if getattr(sys, 'frozen', False):
-    BASE_DIR = Path(sys.executable).resolve().parent
-else:
-    BASE_DIR = Path(__file__).resolve().parent
-
-IMG_PATH = BASE_DIR / "output" / "captura.png"
+pyinstaller --noconfirm --clean --onedir --windowed --name LoginSBS tu_script.py
