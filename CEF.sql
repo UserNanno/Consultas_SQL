dba5f9e2c2be3ba361c6b2e377d2704185f1ea6c
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-25 23:57:06 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,6 +1,10 @@
-from selenium import webdriver
 
-driver = webdriver.Edge()
-driver.get("https://example.com")
-print(driver.title)
-driver.quit()
+(venv) D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs>py test.py
+[44700:31484:1225/235655.192:ERROR:components\edge_ess\vbs_encoder.cc:100] LoadEnclaveImageW failed, error code 577
+
+DevTools listening on ws://127.0.0.1:57098/devtools/browser/45901fc6-4f43-4359-802d-e087cba873df
+Example Domain
+
+(venv) D:\Datos de Usuarios\T72496\Desktop\MODELOS_RPTs>
+
+
