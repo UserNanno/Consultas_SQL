2e596ae8c6ea004882df872db3cedb1a0b70ec57
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-26 09:42:34 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,5 +1,16 @@
+start "" msedge.exe ^
+  --remote-debugging-port=9222 ^
+  --user-data-dir="%~dp0edge_profile"
+
+
+
+
+
+
+
 http://127.0.0.1:9222/json/version
 
 
 
 netstat -ano | findstr :9222
+
