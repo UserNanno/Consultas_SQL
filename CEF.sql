9e8b7252bcd0db3314e599140e511b165292333c
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2025-12-28 01:26:19 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,1 +1,23 @@
-AnalysisException: [UNRESOLVED_COLUMN.WITH_SUGGESTION] A column or function parameter with name `MATSUPERIOR` cannot be resolved. Did you mean one of the following? [`MATSUPERIOR_ORG`, `ETAPA`, `MTOOFERTADO`, `PROCESO`, `TIPACCION`].;
+def add_matsuperior_from_organico(df_final, df_org):
+    df_org_key = (
+        df_org
+        .select(
+            F.col("CODMES").cast("string").alias("CODMESEVALUACION"),
+            F.col("MATORGANICO").alias("MATANALISTA_FINAL"),
+            F.col("MATSUPERIOR").alias("MATSUPERIOR_ORG")
+        )
+        .dropDuplicates(["CODMESEVALUACION", "MATANALISTA_FINAL"])
+    )
+
+    df_out = df_final.join(df_org_key, on=["CODMESEVALUACION", "MATANALISTA_FINAL"], how="left")
+
+    if "MATSUPERIOR" in df_out.columns:
+        df_out = (
+            df_out
+            .withColumn("MATSUPERIOR", F.coalesce(F.col("MATSUPERIOR"), F.col("MATSUPERIOR_ORG")))
+            .drop("MATSUPERIOR_ORG")
+        )
+    else:
+        df_out = df_out.withColumnRenamed("MATSUPERIOR_ORG", "MATSUPERIOR")
+
+    return df_out
