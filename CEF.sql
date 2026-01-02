f20464751ab1208b316d416bb2449cf294d9dae1
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-02 18:26:16 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,5 +1,17 @@
-Traceback (most recent call last):
-  File "main.py", line 79, in <module>
-  File "utils\decorators.py", line 9, in wrapper
-  File "main.py", line 59, in main
-AttributeError: 'RiesgosPage' object has no attribute 'logout_modulo'
+def logout_modulo(self):
+        """
+        1) Click en 'Salir' del módulo /criesgos/logout...
+        2) Espera a que cargue la pantalla siguiente
+        """
+        link = self.wait.until(EC.element_to_be_clickable(self.LINK_SALIR_MODULO))
+        link.click()
+
+        # tras salir del módulo, debería aparecer el botón de logout del portal
+        self.wait.until(EC.presence_of_element_located(self.BTN_SALIR_PORTAL))
+
+    def logout_portal(self):
+        """
+        Click en el botón final del portal: goTo('logout')
+        """
+        btn = self.wait.until(EC.element_to_be_clickable(self.BTN_SALIR_PORTAL))
+        btn.click()
