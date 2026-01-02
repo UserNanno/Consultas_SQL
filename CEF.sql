5a74bb7b292b122c0d758d2ce73b96f38af1a1c1
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-02 18:12:41 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,6 +1,1 @@
-from pathlib import Path
-
-base_excel_path = Path(EXCEL_PATH)
-excel_path = base_excel_path.with_name(
-    f"{base_excel_path.stem}_{dni}{base_excel_path.suffix}"
-)
+pyinstaller --onefile --noconsole ^ --collect-all selenium ^ --add-binary "venv\Lib\site-packages\selenium\webdriver\common\windows\selenium-manager.exe;selenium\webdriver\common\windows" ^ main.py
