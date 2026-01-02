4a3a09216dd5fda86fed9eba2e9c722daf1946ce
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-02 15:59:03 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,51 +1,159 @@
-# pages/riesgos_page.py
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from pages.base_page import BasePage
+<table class="Crw">
+  <colgroup><col width="20%">
+  <col width="18%">
+  <col width="15%">
+  <col width="15%">
+  <col width="17%">
+  <col width="15%">
+  </colgroup><thead>
+    <tr>
+      <td class="Izq" colspan="6">
+        <b class="F">Datos del Deudor</b>
+      </td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="Def">
+      <td class="Izq">
+        <b class="Dz">Documento</b>
+      </td>
+      <td class="Izq Inf">
+        <span class="Dz">
+          DNI
+        </span>
+      </td>
+      <td class="Izq">
+        <b class="Dz">Número</b>
+      </td>
+      <td class="Izq Inf">
+        <span class="Dz">
+          78801600
+        </span>
+      </td>
+      <td class="Izq">
+        <b class="Dz">Persona</b>
+      </td>
+      <td class="Izq Inf">
+        <span class="Dz">
+          Natural
+        </span>
+      </td>
+    </tr>
+    <tr class="Def">
+      <td class="Izq">
+        <b class="Dz">
+          Apellido Paterno
+        </b>
+      </td>
+      <td class="Izq Inf" colspan="3">
+        <span class="Dz">
+          CANECILLAS
+        </span>
+      </td>
+      
+      
+      <td class="Izq">
+        <b class="Dz">N° Entidades</b>
+      </td>
+      <td class="Izq Inf">        
+        <span class="Dz">
+          1
+        </span>
+        
+        <a onclick="verDetallada('3798815671236649815'); return false;" href="#"><img src="images/mas.gif" alt="Entidades"></a>
+        
+      </td>
+      
+      
+    </tr>
+    
+    <tr class="Def">
+      <td class="Izq">
+        <b class="Dz">
+          Apellido Materno
+        </b>
+      </td>
+      <td class="Izq Inf">
+        <span class="Dz">
+          
+          CONTRERAS
+          
+        </span>
+      </td>
+      <td class="Izq">
+        <b class="Dz">
+          Ape. de Casada
+        </b>
+      </td>
+      <td class="Izq Inf">
+        <span class="Dz">
+          
+           
+          
+        </span>
+      </td>
+      <td class="Izq">
+        
+        <b class="Dz">Fecha Reporte</b>
+        
+      </td>
+      <td class="Izq Inf">
+        
+        <b id="FecReporte" class="Dz Red">
+          30/11/2025
+        </b>
+        
+      </td>
+    </tr>
+    <tr class="Def">
+      <td class="Izq">
+        <b class="Dz">
+          Nombres
+        </b>
+      </td>
+      <td class="Izq Inf" colspan="3">
+        <span class="Dz">
+          
+          JUAN MARIANO
+          
+        </span>
+      </td>
+      <td class="Izq">
+        <b class="Dz">Código SBS</b>
+      </td>
+      <td class="Izq Inf">
+        <span class="Dz">
+          226092366
+        </span>
+      </td>
+    </tr>
+    
+    
+    
+    <!-- DISTRIBUCION -->
+    <tr>
+      <td class="Inf" colspan="6">
+        <table class="EE">
+          <tbody><tr>
+            <td class="Izq">
+              <b class="Dz">
+                Distribución Porcentual según Clasificación
+                <sup>1</sup>
+                :
+              </b>
+            </td>
+            <td height="18" width="70"><font class="Dz">0 :100%</font></td><td height="18" width="70"><font class="Dz">1 :0%</font></td><td height="18" width="70"><font class="Dz">2 :0%</font></td><td height="18" width="70"><font class="Dz">3 :0%</font></td><td height="18" width="70"><font class="Dz">4 :0%</font></td>
+          </tr>
+        </tbody></table>
+      </td>
+    </tr>
+    
+    
+    
+  </tbody>
+</table>
 
 
-class RiesgosPage(BasePage):
-
-    def click_link_situacion_deuda(self):
-        """
-        Click en el link:
-        'Aquí Ud. podrá consultar la situación de deuda...'
-        """
-        link = self.wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//a[contains(text(),'consultar la situación de deuda')]")
-            )
-        )
-        link.click()
-
-    def seleccionar_tipo_documento_dni(self):
-        """
-        Selecciona '1. LE/DNI' en el combo
-        """
-        select = self.wait.until(
-            EC.presence_of_element_located((By.ID, "as_tipo_doc"))
-        )
-        Select(select).select_by_value("11")  # DNI
-
-    def ingresar_dni(self, dni: str):
-        """
-        Escribe el DNI
-        """
-        input_dni = self.wait.until(
-            EC.element_to_be_clickable((By.NAME, "as_doc_iden"))
-        )
-        input_dni.clear()
-        input_dni.send_keys(dni)
-
-    def click_consultar(self):
-        """
-        Click en botón Consultar
-        """
-        btn = self.wait.until(
-            EC.element_to_be_clickable((By.ID, "btnConsultar"))
-        )
-        btn.click()
 
 
 
@@ -58,40 +166,91 @@ class RiesgosPage(BasePage):
 
 
 
-from pages.riesgos_page import RiesgosPage
-
-
-
-# === DESPUÉS DEL LOGIN ===
-riesgos_page = RiesgosPage(driver)
-
-# 1) Click en el link de situación de deuda
-riesgos_page.click_link_situacion_deuda()
-
-# 2) Seleccionar DNI
-riesgos_page.seleccionar_tipo_documento_dni()
-
-# 3) Escribir DNI
-riesgos_page.ingresar_dni("78801600")
-
-# 4) Click en Consultar
-riesgos_page.click_consultar()
 
 
 
 
-
-
-
-
-
-
-login_page.fill_form(USUARIO, CLAVE, captcha)
-
-riesgos_page = RiesgosPage(driver)
-riesgos_page.click_link_situacion_deuda()
-riesgos_page.seleccionar_tipo_documento_dni()
-riesgos_page.ingresar_dni("78801600")
-riesgos_page.click_consultar()
-
-print("Flujo completo")
+<table class="Crw">
+  <thead>
+    <tr>
+      <td class="Izq" colspan="4">
+        <span class="F">Posición Consolidada del Deudor</span>
+      </td>
+    </tr>
+  </thead>
+   
+  <tbody>
+    <tr class="Str">
+      <td class="Izq" width="40%">
+        <b class="Dz">SALDOS</b>
+      </td>
+      <td class="Der" width="20%">
+        <b class="Dz">Saldo MN</b>
+      </td>
+      <td class="Der" width="20%">
+        <b class="Dz">Saldo ME (*)</b>
+      </td>
+      <td class="Der" width="20%">
+        <b class="Dz">Total (MN+ ME)</b>
+      </td>
+    </tr>
+    
+    <tr bgcolor="#EAF8FD">
+      <td class="Izq">
+        <span class="Dz">
+          Vigente</span>
+      </td>
+      <td class="Der">
+        <span class="Dz">
+           735</span>
+      </td>
+      <td class="Der">
+        <span class="Dz">
+          0</span>
+      </td>
+      <td class="Der">
+        <span class="Dz">
+           735</span>
+      </td>
+    </tr>
+    
+    <tr bgcolor="#C5EBFA">
+      <td class="Izq">
+        <span class="Dz">
+          TOTAL DEUDA DIRECTA</span>
+      </td>
+      <td class="Der">
+        <span class="Dz">
+           735</span>
+      </td>
+      <td class="Der">
+        <span class="Dz">
+          0</span>
+      </td>
+      <td class="Der">
+        <span class="Dz">
+           735</span>
+      </td>
+    </tr>
+    
+    <tr bgcolor="#62c7ef">
+      <td class="Izq">
+        <span class="Dz">
+          TOTAL DEUDA</span>
+      </td>
+      <td class="Der">
+        <span class="Dz">
+           735</span>
+      </td>
+      <td class="Der">
+        <span class="Dz">
+          0</span>
+      </td>
+      <td class="Der">
+        <span class="Dz">
+           735</span>
+      </td>
+    </tr>
+    
+  </tbody>
+</table>
