cc1e2e2d626241f557238e60ab84bec54fdbe559
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-02 18:28:07 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,17 +1,4 @@
-def logout_modulo(self):
-        """
-        1) Click en 'Salir' del módulo /criesgos/logout...
-        2) Espera a que cargue la pantalla siguiente
-        """
-        link = self.wait.until(EC.element_to_be_clickable(self.LINK_SALIR_MODULO))
-        link.click()
-
-        # tras salir del módulo, debería aparecer el botón de logout del portal
-        self.wait.until(EC.presence_of_element_located(self.BTN_SALIR_PORTAL))
-
-    def logout_portal(self):
-        """
-        Click en el botón final del portal: goTo('logout')
-        """
-        btn = self.wait.until(EC.element_to_be_clickable(self.BTN_SALIR_PORTAL))
-        btn.click()
+pyinstaller --onefile --noconsole ^
+  --collect-all selenium ^
+  --add-binary "D:\Datos de Usuarios\T72496\Desktop\PrismaProject\venv\Lib\site-packages\selenium\webdriver\common\windows\selenium-manager.exe;selenium\webdriver\common\windows" ^
+  main.py
