5635dcadd89aa241fd4ed793f74d6b0d7ea1a682
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-02 18:20:54 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,4 +1,4 @@
-(venv) D:\Datos de Usuarios\T72496\Desktop\PrismaProject>python -c "import selenium; from pathlib import Path; p=Path(selenium.__file__).resolve().parent/'webdriver'/'common'/'windows'/'selenium-manager.exe'; print(p)"
-D:\Datos de Usuarios\T72496\Desktop\PrismaProject\venv\Lib\site-packages\selenium\webdriver\common\windows\selenium-manager.exe
-
-(venv) D:\Datos de Usuarios\T72496\Desktop\PrismaProject>
+pyinstaller --onefile --noconsole ^
+  --collect-all selenium ^
+  --add-binary "D:\Datos de Usuarios\T72496\Desktop\PrismaProject\venv\Lib\site-packages\selenium\webdriver\common\windows\selenium-manager.exe;selenium\webdriver\common\windows" ^
+  main.py
