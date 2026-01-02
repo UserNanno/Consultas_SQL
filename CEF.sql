cb2a028d3390714ff0e37ab7373a071216f29d23
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-02 18:18:50 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,1 +1,4 @@
-python -c "import selenium; from pathlib import Path; p=Path(selenium.__file__).resolve().parent/'webdriver'/'common'/'windows'/'selenium-manager.exe'; print(p)"
+(venv) D:\Datos de Usuarios\T72496\Desktop\PrismaProject>python -c "import selenium; from pathlib import Path; p=Path(selenium.__file__).resolve().parent/'webdriver'/'common'/'windows'/'selenium-manager.exe'; print(p)"
+D:\Datos de Usuarios\T72496\Desktop\PrismaProject\venv\Lib\site-packages\selenium\webdriver\common\windows\selenium-manager.exe
+
+(venv) D:\Datos de Usuarios\T72496\Desktop\PrismaProject>
