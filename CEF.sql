3ad5bc4a68f2a79d37dff010692b76636ad26842
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-02 15:41:22 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,47 +1,97 @@
-Cuando me carga la pagina despues del login quisiera que de click a este boton
-
-<tr>
-              <td class="Izq" valign="top">
-                <a onclick="f_hub('/criesgos/criesgos/criesgos.jsp', '/criesgos/login', '00002', '_self', 'S'); return false;" href="#" class="descripcion">Aquí Ud. podrá consultar la situación de deuda de los clientes del sistema financiero, así mismo Ud. podra ver el histórico de dicha información.</a>
-              </td>
-            </tr>
+# pages/riesgos_page.py
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from pages.base_page import BasePage
 
 
+class RiesgosPage(BasePage):
+
+    def click_link_situacion_deuda(self):
+        """
+        Click en el link:
+        'Aquí Ud. podrá consultar la situación de deuda...'
+        """
+        link = self.wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//a[contains(text(),'consultar la situación de deuda')]")
+            )
+        )
+        link.click()
+
+    def seleccionar_tipo_documento_dni(self):
+        """
+        Selecciona '1. LE/DNI' en el combo
+        """
+        select = self.wait.until(
+            EC.presence_of_element_located((By.ID, "as_tipo_doc"))
+        )
+        Select(select).select_by_value("11")  # DNI
+
+    def ingresar_dni(self, dni: str):
+        """
+        Escribe el DNI
+        """
+        input_dni = self.wait.until(
+            EC.element_to_be_clickable((By.NAME, "as_doc_iden"))
+        )
+        input_dni.clear()
+        input_dni.send_keys(dni)
+
+    def click_consultar(self):
+        """
+        Click en botón Consultar
+        """
+        btn = self.wait.until(
+            EC.element_to_be_clickable((By.ID, "btnConsultar"))
+        )
+        btn.click()
 
 
 
-Eso me llevara a otra pagina donde el tendrá un formulario dentro hay tipo un listbox donde quisiwera que seleccionara el DNI
-
-<select name="as_tipo_doc" id="as_tipo_doc" style="width: 200px;">
-                  <option value="*">Tipo de Documento</option>
-                  <option value="11" selected="selected">1. LE/DNI</option>
-                  <option value="12">2. Carnet de Extranjería</option>
-                  <option value="13">3. Carnet de Identidad FFPP</option>
-                  <option value="14">4. Carnet de Identidad FFAA</option>
-                  <option value="15">5. Pasaporte</option>
-                  <option value="21">6. RUC</option>
-                  
-                  <option value="17">7. Carnet de Identidad emitido por el Ministerio de Relaciones Exteriores</option>
-                  <option value="18">8. Carnet de Permiso Temporal de Permanencia</option>
-                  <option value="00">9. Código SBS</option>
-                  
-                  <option value="1A">A. Cédula de identidad o documentos análogos</option>
-                  <option value="1B">B. Carnet del refugiado</option>
-                  <option value="1C">C. Documento expedido por la CEPR del Ministerio de Relaciones Exteriores
-                    que acredita que la solicitud de refugiado se encuentra en trámite</option>
-                </select>
 
 
 
-Luego que seleccionara
-
-Que escriba el siguiente DNI 78801600 acá
-
-
-<input type="text" size="15" maxlength="16" class="input-upper" onchange="ValInputValid(this)" onkeypress="return OnKeyPressValidInput(event)" name="as_doc_iden" value="Número de Doc." onclick="this.select(); this.focus();">
 
 
 
-Luego de ello que le de click aquí
 
-<input type="submit" name="Submit" value="Consultar" class="boton" id="btnConsultar">
+
+
+from pages.riesgos_page import RiesgosPage
+
+
+
+# === DESPUÉS DEL LOGIN ===
+riesgos_page = RiesgosPage(driver)
+
+# 1) Click en el link de situación de deuda
+riesgos_page.click_link_situacion_deuda()
+
+# 2) Seleccionar DNI
+riesgos_page.seleccionar_tipo_documento_dni()
+
+# 3) Escribir DNI
+riesgos_page.ingresar_dni("78801600")
+
+# 4) Click en Consultar
+riesgos_page.click_consultar()
+
+
+
+
+
+
+
+
+
+
+login_page.fill_form(USUARIO, CLAVE, captcha)
+
+riesgos_page = RiesgosPage(driver)
+riesgos_page.click_link_situacion_deuda()
+riesgos_page.seleccionar_tipo_documento_dni()
+riesgos_page.ingresar_dni("78801600")
+riesgos_page.click_consultar()
+
+print("Flujo completo")
