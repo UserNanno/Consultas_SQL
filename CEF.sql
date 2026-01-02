3a990bd6ed439a9ff13287840fb47c314d1a8227
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-02 18:13:03 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,1 +1,4 @@
-pyinstaller --onefile --noconsole ^ --collect-all selenium ^ --add-binary "venv\Lib\site-packages\selenium\webdriver\common\windows\selenium-manager.exe;selenium\webdriver\common\windows" ^ main.py
+pyinstaller --onefile --noconsole ^
+  --collect-all selenium ^
+  --add-binary "...\selenium-manager.exe;selenium\webdriver\common\windows" ^
+  main.py
