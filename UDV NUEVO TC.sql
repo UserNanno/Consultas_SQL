e73453eb9ee6bd6fd5283aa7d2353fb7e4a1b1c6
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-05 13:22:24 -0500

    Update UDV NUEVO TC.sql

--- UDV NUEVO TC.sql
+++ UDV NUEVO TC.sql
@@ -1,9 +1,49 @@
- El excel que quiero abrir es Macro.xlsm que se encuentra en D:\Datos de Usuarios\T72496\Desktop\PrismaProject
+Perfecto ya sale tal cual queria pero ahora tengo que cambiar un poco de que va a tomar captura. 
 
-Donde dentro hay varias hojas, por ejemplo en la hoja "SBS" hay un boton integrado con una MACRO me imagino que le das click y se pega la imagen, desde navegación me sale en la posicion SBS!C5
+ Antes del logout entraba a cierta seccion cuando buscaba por DNI. Es decir a penas entraba y ya extraiamos la informaciom, tomabamos la captura de la pagina y luego la captura lo pegabamos en el excel.
 
-Podemos tener la captura y pegarlo ahi? o sin usar el boton. Normalmente se pega de C5 hasta Z50 (TODO ESE CUADRANTE)
+ Pero aca quiero hacer un cambio, una vez que busque el DNI deberá darle click al boton:
+       <li>
+          <a id="idOp4" href="#" title="Información Detallada">Detallada</a>
+      </li>
 
-y quisiera que tome la captura antes de hacer logout
+ Que se encuentra dentro de este DIV:
+ 
+<div id="Menu">
+  <ul class="horizontal">
+      <li>
+          <a id="idOp0" href="#" title="Posición Consolidada del Deudor">Consolidado</a>
+      </li>
+      <li>
+          <a id="idOp1" href="#" title="Posición del Deudor por Tipo de Crédito">Por Tipo de Credito</a>
+      </li>
+      <li>
+          <a id="idOp2" href="#" title="Información Histórica">Historica</a>
+      </li>
+      <li>
+          <a id="idOp3" href="#" title="Información Adicional">Adicional</a>
+      </li>
+      <li>
+          <a id="idOp4" href="#" title="Información Detallada">Detallada</a>
+      </li>
+      <li>
+          <a id="idOp5" href="#" title="Reporte preparado para el Cliente">Al Cliente</a>
+      </li>
+      <li>
+          <a id="idOp6" href="#" title="Información referida a otros reportes">Otros Reportes</a>
+      </li>
+      <li>
+          <span>RIO</span>
+      </li>
+  </ul>
+</div>
 
-Se podrá? 
+Tomar la captura y luego pegarla en C64 hasta Z110 es decir C64:Z110
+
+
+Posterior debera elegir el botón 
+      <li>
+          <a id="idOp6" href="#" title="Información referida a otros reportes">Otros Reportes</a>
+      </li>
+
+tomar la captura y pegarlo en la C5:Z50
