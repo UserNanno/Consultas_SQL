f18e265f7040d0c934473bfb479e2c6d346783d7
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-07 19:17:55 -0500

    Update UDV_NUEVO_CEF.sql

--- UDV_NUEVO_CEF.sql
+++ UDV_NUEVO_CEF.sql
@@ -1,7 +1,13 @@
-(venv) D:\Datos de Usuarios\T72496\Desktop\PrismaProject\app>py gui_app.py
+(venv) D:\Datos de Usuarios\T72496\Desktop\PrismaProject>py -m app.gui_app.py
 Traceback (most recent call last):
+  File "<frozen runpy>", line 189, in _run_module_as_main
+  File "<frozen runpy>", line 112, in _get_module_details
   File "D:\Datos de Usuarios\T72496\Desktop\PrismaProject\app\gui_app.py", line 1, in <module>
     from ui.main_windows import MainWindow
-ModuleNotFoundError: No module named 'ui'
+  File "D:\Datos de Usuarios\T72496\Desktop\PrismaProject\ui\main_windows.py", line 3, in <module>
+    from controllers.consulta_controller import ConsultaController
+  File "D:\Datos de Usuarios\T72496\Desktop\PrismaProject\controllers\consulta_controller.py", line 5, in <module>
+    from main import run_app
+ImportError: cannot import name 'run_app' from 'main' (D:\Datos de Usuarios\T72496\Desktop\PrismaProject\main.py)
 
-(venv) D:\Datos de Usuarios\T72496\Desktop\PrismaProject\app>
+(venv) D:\Datos de Usuarios\T72496\Desktop\PrismaProject>
