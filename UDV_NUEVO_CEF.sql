1224db3884a14dc31fc29cc123b95388a431396a
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-08 18:16:07 -0500

    Update UDV_NUEVO_CEF.sql

--- UDV_NUEVO_CEF.sql
+++ UDV_NUEVO_CEF.sql
@@ -1,37 +1,10 @@
-                                                <div class="col" style="flex-grow:1">
-                                                    <h5 class="editor-label" style="font-style: oblique;">5. Scores de Crédito Efectivo y Convenio</h5>
-                                                    <ul class="list-group" style="margin-right: 50px; color: #1F497D;">
                                                         <li class="list-group-item d-flex justify-content-between align-items-center" style="background-color: #DCE6F1;">
-                                                            <div class="d-flex align-items-center" style="cursor: pointer;" data-toggle="tooltip" data-placement="top" title="" data-original-title="Evalúa si el cliente tiene el perfil para recibir un Credito en Efectivo de Libre Disponibilidad, según su historial y comportamiento financiero.">
-                                                                Venta CEF LD/RE 
+                                                            <div class="d-flex align-items-center" style="cursor: pointer;" data-toggle="tooltip" data-placement="top" title="" data-original-title="Evalúa si el cliente tiene perfil para recibir una nueva tarjeta de crédito, según su historial y comportamiento financiero.">
+                                                                Venta TC Nueva 
                                                             </div>
                                                                 <span class="badge badge-primary badge-pill">
 <span style="cursor:pointer;" data-toggle="tooltip" data-placement="top" title="" data-original-title="Puntaje interno">
-                                                                        252
+                                                                        353
                                                                     </span>
                                                                 </span>
 </li>
-                                                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                                                            <div class="d-flex align-items-center" style="cursor: pointer;" data-toggle="tooltip" data-placement="top" title="" data-original-title="Evalúa si el cliente tiene el perfil para recibir un Credito en Efectivo Compra de Deuda, según su historial y comportamiento financiero.">
-                                                                Venta CEF CdD 
-                                                            </div>
-                                                                <span class="badge badge-primary badge-pill">
-<span style="cursor:pointer;" data-toggle="tooltip" data-placement="top" title="" data-original-title="Puntaje interno">
-                                                                        301
-                                                                    </span>
-                                                                </span>
-</li>
-                                                        <li class="list-group-item d-flex justify-content-between align-items-center" style="background-color: #DCE6F1;">
-                                                            <div class="d-flex align-items-center" style="cursor: pointer;" data-toggle="tooltip" data-placement="top" title="" data-original-title="Revisa el comportamiento del cliente en créditos efectivos, considerando su manejo y perfil, con o sin score asociado.">
-                                                                Portafolio CEF (Score BHV) 
-                                                            </div>
-                                                                <span class="badge badge-primary badge-pill">
-<span style="cursor:pointer;" data-toggle="tooltip" data-placement="top" title="" data-original-title="Puntaje interno">
-                                                                        314
-                                                                    </span>
-                                                                </span>
-</li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
