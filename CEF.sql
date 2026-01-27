db6bb3363c0a8c9a5a44283d27298c20654bca80
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-01-27 14:49:08 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,6 +1,2 @@
-def limpiar_cesado(col):
-    c = col.cast("string")
-    # borra "(CESADO)" o "CESADO" como token
-    c = F.regexp_replace(c, r"(?i)\(?\s*CESADO\s*\)?", "")
-    c = F.regexp_replace(c, r"\s+", " ")
-    return F.trim(c)
+    paso_tc_analista  = (F.col("NBRPASO") == "APROBACION DE CREDITOS ANALISTA")
+    paso_cef_analista = (F.col("NBRPASO") == "EVALUACION DE SOLICITUD")
