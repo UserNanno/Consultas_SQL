58a4ceb7fe9640e3c2ef08d81364488a7dbe6ffd
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-02-13 19:03:54 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,9 +1,11 @@
-FECHA_MAX = 
-"ACTUALIZADO AL" & UNICHAR(10) & UNICHAR(10) & 
-FORMAT(
+# DIAS UTILES DEL MES ACTUAL =
+VAR MesActual = MAX(Fact[CODMES])
+VAR DiasUtiles =
     CALCULATE(
-        MAX(WEEKLY[FECSOLICITUD]), 
-        ALL(WEEKLY)
-    ), 
-    "DD-MM-YYYY"
-)
+        MAX(Fact[ACUM_DIA_UTIL_EQ_MES]),
+        Fact[CODMES] = MesActual
+    )
+RETURN
+"# DIAS UTILES DEL MES ACTUAL" 
+& UNICHAR(10) & UNICHAR(10) &
+FORMAT(DiasUtiles, "0.0")
