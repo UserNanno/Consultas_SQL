8562448653b4b697fd7995785a647b461c376604
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-02-24 11:43:55 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,12 +1,14 @@
 # EJECUCIÓN
-df_org = load_organico(spark, BASE_DIR_ORGANICO)
+df_org = load_organico(spark, BASE_DIR_ORGANICO).cache()
+df_org.count()   # materializa lectura + normalización
 
 df_diccionario = build_diccionario_actores(
     spark,
     path_estados=PATH_SF_ESTADOS,
     path_productos=PATH_SF_PRODUCTOS,
     filtrar_stopwords=True
-)
+).cache()
+df_diccionario.count()  # materializa lectura SF + tokens
 
 matches_top = map_diccionario_a_organico(
     df_diccionario=df_diccionario,
@@ -15,9 +17,11 @@ matches_top = map_diccionario_a_organico(
     usar_stopwords=True,
     alinear_mes=True,
     devolver_todos=False
-)
+).cache()
+matches_top.count()  # materializa el join pesado de tokens UNA vez
 
-df_pa = load_powerapps(spark, PATH_PA_SOLICITUDES)
+df_pa = load_powerapps(spark, PATH_PA_SOLICITUDES).cache()
+df_pa.count()  # materializa lectura/dedup de PA
 
 # Incorporar solo los analistas que aparecen por correo en PowerApps y no existían ya en matches_top
 matches_final = incorporar_powerapps_en_matches(
@@ -26,3 +30,7 @@ matches_final = incorporar_powerapps_en_matches(
     df_org=df_org,
     alinear_mes=True
 )
+
+# (opcional) si vas a usar matches_final varias veces:
+# matches_final = matches_final.cache()
+# matches_final.count()
