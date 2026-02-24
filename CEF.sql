ba2a529b27d4ce18fea65e2c8341a216637e1e6a
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-02-24 12:15:23 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,5 +1,23 @@
-Asi vienen
+def parse_created_utc(col):
+    """
+    Parsea PowerApps Created en formato:
+    - 2026-02-02T15:02:04Z
+    - 2026-02-02T15:02:04.000Z (por si aparece)
+    Devuelve timestamp en UTC.
+    """
+    s = col.cast("string")
 
-2026-02-02T15:02:04Z
+    return F.coalesce(
+        F.to_timestamp(s, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
+        F.to_timestamp(s, "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    )
 
-2026-02-14T14:54:22Z
+
+
+
+df = df.withColumn("CREATED_TS_UTC", parse_created_utc(F.col("CREATED")))
+
+df = df.withColumn(
+    "CREATED_LIMA",
+    F.from_utc_timestamp(F.col("CREATED_TS_UTC"), "America/Lima")
+)
