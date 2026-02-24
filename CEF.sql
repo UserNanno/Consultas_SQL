cf6232d1e216636b8b458c9457584f61ca9767d0
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-02-24 12:21:29 -0500

    Update CEF.sql

--- CEF.sql
+++ CEF.sql
@@ -1,5 +1,37 @@
-Podemos hacer que quede FECHORACREACION asi 2026-01-21 14:11:00 porque actualmente me sale 2026-01-21T14:11:00.000+00:00
+# FECHORACREACION_TS = timestamp real (para lógica)
+df = df.withColumn("FECHORACREACION_TS", F.date_trunc("minute", F.col("CREATED_LIMA")))
 
-También quiero que al final el orden de las columnas sea asi:
-    
-CODMES;CODSOLICITUD;FECHORACREACION;FECCREACION;HORACREACION;MATANALISTA;FECASIGNACION;PRODUCTO;RESULTADOANALISTA;MOTIVORESULTADOANALISTA;MOTIVOMALADERIVACION;SUBMOTIVOMALADERIVACION
+# FECHORACREACION = string bonito para export
+df = df.withColumn("FECHORACREACION", F.date_format(F.col("FECHORACREACION_TS"), "yyyy-MM-dd HH:mm:ss"))
+
+# FECCREACION/HORACREACION desde el TS
+df = (
+    df.withColumn("FECCREACION", F.to_date("FECHORACREACION_TS"))
+      .withColumn("HORACREACION", F.date_format("FECHORACREACION_TS", "HH:mm:ss"))
+)
+
+
+pick = [c for c in ["CODMES","CORREO","MATORGANICO","MATSUPERIOR"] if c in cols]
+
+df_apps_enriq = (
+    df_apps_key.alias("a")
+    .join(df_org.alias("o"), on=["CODMES","CORREO"], how="left")
+    .withColumnRenamed("MATORGANICO", "MATANALISTA")
+)
+
+cols_final = [
+    "CODMES",
+    "CODSOLICITUD",
+    "FECHORACREACION",
+    "FECCREACION",
+    "HORACREACION",
+    "MATANALISTA",
+    "FECASIGNACION",
+    "PRODUCTO",
+    "RESULTADOANALISTA",
+    "MOTIVORESULTADOANALISTA",
+    "MOTIVOMALADERIVACION",
+    "SUBMOTIVOMALADERIVACION",
+]
+
+df_apps_final = df_apps_enriq.select(*cols_final)
