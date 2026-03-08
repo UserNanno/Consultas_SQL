57681e5d7f75ad11d62404a670b99c0001c3d243
Juan Mariano Canecillas Contreras <99894384+UserNanno@users.noreply.github.com>
2026-03-08 11:27:49 -0500

    Update Mejoras.py

--- Mejoras.py
+++ Mejoras.py
@@ -1,107 +1,129 @@
-Quiero ejecutar esto desde databricks
+Mira me compartieron esto en pyspark
 
-evaluaciones = spark.sql("""
-    SELECT
-        A.numsolicitudevaluacion,
-        A.codmesevaluacion,
-        A.fecevaluacion,
-        A.codclavepartycli as codclavepartycli_a,
-        A.descanalventasolicitudrbm as canal_a,
-        CASE WHEN A.destipevaluacionsolicitudcredito = 'TC NUEVA' THEN 'TC Nueva'
-            WHEN A.destipevaluacionsolicitudcredito = 'EP' THEN 'PTC'
-            WHEN A.destipevaluacionsolicitudcredito = 'BT' THEN 'BT'
-            ELSE A.destipevaluacionsolicitudcredito
-            END AS producto_a,
-        A.mtoingresoevaluacion AS ingresoevaluacion,
-        A.mtocemevaluacion AS cemevaluacion,
-        
-        --CUADRE DATA
-        A.codevaluacionsolicitud,
-        A.desevaluacion AS flujoevaluacion,
-        A.tipdecisionresultadorbm AS tipdecision,
-        A.destipdecisionresultadorbm AS destipdecision,
-        A.desdecisionevaluacion AS detallemotivo,
-        A.desreglapauta AS reglamotivo,
-        A.codcanalventasolicitudrbm AS codcanalevaluacion,
-        A.descampaniasolicitud AS tipcampania,
-        CASE WHEN A.descampaniasolicitud IN ('100% Aprobado','Pre-Aprobado') THEN 1
-            ELSE 0
-            END AS flgcampania,
+solicitudes_consolidado = spark.sql(f"""Select codmesevaluacion,
+                codevaluacionsolicitud as codevaluacion,
+                codidevaluacion as codevaluacionlargo,
+                fecevaluacion,
+                codinternocomputacional,
+                --codinternocomputacionalconyuge,
+                --case when codinternocomputacionalconyuge is not null then 'CAS'
+                     --else 'SOL' end as estadocivil,
+                numsolicitudevaluacion as numsolicitudprestamo,
+                codsecuencialdecisionrbm as numdecision,
+                desdecisionevaluacion as decision,
+                desreglapauta as decisionregla,
+                destipdecisionresultadorbm as resultadocda,
+                desevaluacion as flujoevaluacion,
+                mtocemevaluacion as mtocapacidadmaxendeudamiento,
+                descampaniasolicitud as descampana,
+                destipevaluacionsolicitudcredito as descampanaagrupado,
+                descanalventarbmper as canalventarbm,
+                
+                case when descanalventarbmper = 'SALEFORCE' then SUBSTR(trim(numsolicitudevaluacion),5,7)
+                     when descanalventarbmper = 'LOANS' then SUBSTR(trim(numsolicitudevaluacion),3,8)
+                     else trim(numsolicitudevaluacion) end as numsolicitudcorto,
 
-        /*------------------------------------------------------------SALESFORCE------------------------------------------------------------*/
-        CASE WHEN B.codsolicitudappriesgo IS NOT NULL THEN 1
-            ELSE 0
-            END AS flgcrucesalesforce,
-        B.codsolicitud AS codsolicitudsalesforce, --cuadre
+                case when destipdecisionresultadorbm = 'Approve' then 1
+                     when destipdecisionresultadorbm = 'Decline' then 3
+                     else 2 end as resultadojerarq,
 
-        B.codclavepartycli as codclavepartycli_b,
-        CASE WHEN B.codapp = 'SCRM' THEN 'SALEFORCE'
-            WHEN B.codapp = 'XT21' THEN 'CARDS'
-            END AS canal_b,
-        CASE WHEN B.destipflujoventatarjetacredito IN ('TC', 'SEGUNDA TC') THEN 'TC Nueva'
-            WHEN B.destipflujoventatarjetacredito IN ('AMPLIACION') THEN 'Ampliacion'
-            WHEN B.destipflujoventatarjetacredito IN ('ADICIONAL') THEN 'Adicional'
-            WHEN B.destipflujoventatarjetacredito IN ('UPGRADE','UPGRATE') THEN 'Upgrade'
-            WHEN B.destipflujoventatarjetacredito IN ('EP') THEN 'PTC'
-            ELSE B.destipflujoventatarjetacredito
-            END AS producto_b,
-        B.destipetapa,            --INICIADO/SOLICITUD/EVALUACION/OPERACIONES-VENTAS/APROBACION DE OPERACIONES/RECHAZADA/CERRADA/DESESTIMADA/null (4905)
-        CASE WHEN B.destipetapa = 'CERRADA' THEN '1.CERRADA'
-            WHEN B.destipetapa = 'DESESTIMADA' THEN '2.DESESTIMADA'
-            ELSE '3.OTRO'
-            END AS destipetapa_f,
-        B.tipevaluacionsolicitud, --EVAU/EVCE/null (970222)
-        B.flgestadocda,           --N/S/null (4789)
-        B.mtoaprobado,
+                case when descanalventarbmper = 'BANCA MÓVIL' and desevaluacion = 'df_Regular' then '0.No valido'
+                     when descampaniasolicitud = 'Reactivo' then '1.Reactivo'
+                     else '2.No Reactivo' end as evaluacionreactivo,
 
-        /*------------------------------------------------------------CENTRALIZADO------------------------------------------------------------*/
-        CASE WHEN C.nrosolicitud IS NOT NULL THEN 1
-            ELSE 0
-            END AS flgcentralizado
-    FROM catalog_lhcl_prod_bcp.bcp_ddv_rbmrbmper_modelogestion_vu.md_evaluacionsolicitudcredito A
-    LEFT JOIN catalog_lhcl_prod_bcp.bcp_udv_int_vu.m_solicitudtarjetacredito B                                  --salesforce
-    ON A.codidevaluacion = SUBSTRING(B.codsolicitudappriesgo, 1, LENGTH(B.codsolicitudappriesgo) - 2)
-    LEFT JOIN (SELECT DISTINCT nrosolicitud FROM catalog_lhcl_prod_bcp.bcp_edv_rbmper.t57182_centralizado) C    --centralizado
-    ON B.codsolicitud = C.nrosolicitud
-    WHERE A.flgregeliminado = 'N' AND A.tipproductosolicitudrbm = 'TC'
-    """)
+                case when destipevaluacionsolicitudcredito in ('Regular LD', 'Cuotealo') and descampaniasolicitud in ('100% Aprobado', 'Pre-Aprobado') then 'LD APR'
+                     when destipevaluacionsolicitudcredito = 'Regular LD' and descampaniasolicitud = 'Convenio' then 'LD CONV'
+                     when destipevaluacionsolicitudcredito = 'Regular LD' and descampaniasolicitud = 'Invitado' then 'LD INV'
+                     when destipevaluacionsolicitudcredito in ('Regular LD', 'Cuotealo') and descampaniasolicitud = 'CEF Shield' then 'LD SHD'
 
-#evaluaciones_fuente = spark.sql("""select * from catalog_lhcl_prod_bcp.bcp_ddv_rbmrbmper_modelogestion_vu.md_evaluacionsolicitudcredito
-#                                WHERE flgregeliminado = 'N' AND tipproductosolicitudrbm = 'TC'""")
-#print(f"Registros fuente: {evaluaciones_fuente.count()}")
+                     when destipevaluacionsolicitudcredito = 'Compra de Deuda' and descampaniasolicitud in ('100% Aprobado', 'Pre-Aprobado') then 'CDD APR'
+                     when destipevaluacionsolicitudcredito = 'Compra de Deuda' and descampaniasolicitud = 'Convenio' then 'CDD CONV'
+                     
+                     when destipevaluacionsolicitudcredito = 'LD + Consolidación' and descampaniasolicitud in ('100% Aprobado', 'Pre-Aprobado') then 'REE APR'
+                     when destipevaluacionsolicitudcredito = 'LD + Consolidación' and descampaniasolicitud = 'Convenio' then 'REE CONV'
 
-#print(f"Registros base: {evaluaciones.count()}")
-#display(evaluaciones)
-evaluaciones.createOrReplaceTempView("evaluaciones")
+                     when destipevaluacionsolicitudcredito = 'LD + Compra de Deuda + Consolidación' and descampaniasolicitud in ('100% Aprobado', 'Pre-Aprobado') then 'CONS APR'
+                     when destipevaluacionsolicitudcredito = 'LD + Compra de Deuda + Consolidación' and descampaniasolicitud = 'Convenio' then 'CONS CONV'
 
-PERO ME SALE ERROR
+                     else 'REACTIVO' end as LEAD_CEF
+        from    catalog_lhcl_prod_bcp.bcp_ddv_rbmrbmper_modelogestion_vu.md_evaluacionsolicitudcredito
+        where   tipproductosolicitudrbm = 'CC'
+        and     descanalventarbmper not in ('YAPE', 'OTROS')
+        and     codmesevaluacion = {codmes}""")
 
-AnalysisException: [INSUFFICIENT_PERMISSIONS] Insufficient privileges:
-User does not have USE SCHEMA on Schema 'catalog_lhcl_prod_bcp.bcp_edv_rbmper'.
+validacion_1=solicitudes_consolidado.groupBy("codinternocomputacional", "codevaluacion").count().withColumnRenamed("count","conteo")
+validacion_conteo=validacion_1.filter((validacion_1.conteo>1))
+print(validacion_conteo.count())
+
+if validacion_conteo.count()>0:
+    print("Ejecutando limpieza de duplicados")
+    windowSpec = Window.partitionBy("codinternocomputacional", "codevaluacion").orderBy("codinternocomputacional")
+    
+    # Agregar una columna de número de fila para cada grupo de duplicados
+    solicitudes_consolidado = solicitudes_consolidado.withColumn("row_number", row_number().over(windowSpec))
+    
+    # Filtrar solo los registros con row_number igual a 1
+    solicitudes_consolidado = solicitudes_consolidado.filter("row_number = 1").drop("row_number")
+
+else:
+    print('sin duplicados')
+
+#display(solicitudes_consolidado.limit(2))
+solicitudes_consolidado.createOrReplaceTempView("df_solicitudes_consolidado")
 
 
+Pero quiero hacerlo en SQL nomas
 
-pero necesito permisos y para ello uso esto
+Ya tengo
 
-%pip install --upgrade databricks-sql-connector
-dbutils.library.restartPython()  # o reinicia el cluster/kernel manualmente
+SELECT
+	CODMESEVALUACION,
+	CODEVALUACIONSOLICITUD,
+	CODIDEVALUACION,
+	FECEVALUACION,
+	CODINTERNOCOMPUTACIONAL,
+	NUMSOLICITUDEVALUACION,
+	CODSECUENCIALDECISIONRBM,
+	DESDECISIONEVALUACION,
+	DESREGLAPAUTA,
+	DESTIPDECISIONRESULTADORBM,
+	DESEVALUACION,
+	MTOCEMEVALUACION,
+	DESCAMPANIASOLICITUD,
+	DESTIPEVALUACIONSOLICITUDCREDITO,
+	DESCANALVENTARBMPER,
+	(CASE
+		WHEN DESCANALVENTARBMPER = 'SALEFORCE' THEN SUBSTR(TRIM(NUMSOLICITUDEVALUACION), 5, 7)
+		WHEN DESCANALVENTARBMPER = 'LOANS' THEN SUBSTR(TRIM(NUMSOLICITUDEVALUACION), 3, 8)
+		ELSE TRIM(NUMSOLICITUDEVALUACION)
+	END) AS NUMSOLICITUDCORTO,
+	(CASE
+		WHEN DESTIPDECISIONRESULTADORBM = 'Approve' THEN 1
+		WHEN DESTIPDECISIONRESULTADORBM = 'Decline' THEN 3
+		ELSE 2
+	END) AS RESULTADOJERARQUIA,
+	(CASE
+		WHEN DESCANALVENTARBMPER = 'BANCA MÓVIL' AND DESEVALUACION = 'df_Regular' THEN 'NO VALIDO'
+		WHEN DESCAMPANIASOLICITUD = 'Reactivo' THEN 'REACTIVO'
+		ELSE 'NO REACTIVO'
+	END) AS EVALUACIONREACTIVO,
+	(CASE
+		WHEN DESTIPEVALUACIONSOLICITUDCREDITO IN ('REGULAR LD', 'CUOTEALO') AND DESCAMPANIASOLICITUD IN ('100% Aprobado', 'Pre-Aprobado') THEN 'LD APROBADO'
+		WHEN DESTIPEVALUACIONSOLICITUDCREDITO IN ('REGULAR LD', 'CUOTEALO') AND DESCAMPANIASOLICITUD = 'CEF Shield' THEN 'LD SHIELD'
+		WHEN DESTIPEVALUACIONSOLICITUDCREDITO = 'REGULAR LD' AND DESCAMPANIASOLICITUD = 'Convenio' THEN 'LD CONVENIO'
+		WHEN DESTIPEVALUACIONSOLICITUDCREDITO = 'COMPRA DE DEUDA' AND DESCAMPANIASOLICITUD IN ('100% Aprobado', 'Pre-Aprobado') THEN 'CDD APROBADO'
+		WHEN DESTIPEVALUACIONSOLICITUDCREDITO = 'COMPRA DE DEUDA' AND DESCAMPANIASOLICITUD = 'Convenio' THEN 'CDD CONVENIO'
+		WHEN DESTIPEVALUACIONSOLICITUDCREDITO = 'LD + CONSOLIDACION' AND DESCAMPANIASOLICITUD IN ('100% Aprobado', 'Pre-Aprobado') THEN 'REE APROBADO'
+		WHEN DESTIPEVALUACIONSOLICITUDCREDITO = 'LD + CONSOLIDACION' AND DESCAMPANIASOLICITUD = 'Convenio' THEN 'REE CONVENIO'
+		WHEN DESTIPEVALUACIONSOLICITUDCREDITO = 'LD + COMPRA DE DEUDA + CONSOLIDACION' AND DESCAMPANIASOLICITUD IN ('100% Aprobado', 'Pre-Aprobado') THEN 'CONS APROBADO'
+		WHEN DESTIPEVALUACIONSOLICITUDCREDITO = 'LD + COMPRA DE DEUDA + CONSOLIDACION' AND DESCAMPANIASOLICITUD = 'Convenio' THEN 'CONS CONVENIO'
+		ELSE 'REACTIVO'
+	END) AS LEAD_CEF
+FROM CATALOG_LHCL_PROD_BCP.BCP_DDV_RBMRBMPER_MODELOGESTION_VU.MD_EVALUACIONSOLICITUDCREDITO
+WHERE TIPPRODUCTOSOLICITUDRBM = 'CC' AND DESCANALVENTARBMPER NOT IN ('YAPE', 'OTROS') AND CODMESEVALUACION = 202001;
 
+y QUIERO hacer lo de saber si hay duplicados por CODINTERNOCOMPUTACIONAL y CODEVALUACIONSOLICITUD asi como se hacia en pypsark.
 
-from databricks import sql
-import os
+Como seria?
 
-connection = sql.connect(
-                        server_hostname = "adb-6238163592670798.18.azuredatabricks.net",
-                        http_path = "/sql/1.0/warehouses/510f1d9e2d1b2250",
-                        access_token = "<access-token>")
-
-cursor = connection.cursor()
-
-cursor.execute("SELECT * from range(10)")
-print(cursor.fetchall())
-
-cursor.close()
-connection.close()
-
-
-Cuando ejecuto esto ya me funciona leer los catalogos
+Actualmente se hace para 1 mes, como sería para todos los meses que yo diga? 
